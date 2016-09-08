888c2d955d249995e1cb4519a53847c34baae088
Radek Poleski <radek.poleski@gmail.com>
2016-09-08 17:47:52 -0400

    use case #4 corrected

--- examples/use_cases/use_case_04_basic_event_properties.py
+++ examples/use_cases/use_case_04_basic_event_properties.py
@@ -9,9 +9,9 @@ e.model = MulensModel.Model(...)    # define in one of the ways from previous us
 e.ra = 270.12345 * u.deg
 e.dec = -27.6789 * u.deg
 
-e.datasets.append(np.array(hjd), np.array(m), np.array(e))    # where t, m, e are previously read/defined numpy arrays
+e.datasets.append(MulensModel.UlensData(np.array(hjd), np.array(m), np.array(e)))    # where t, m, e are previously read/defined numpy arrays
 
-e.datasets.append(file_name='XXX.dat', format='OGLE', bandpass='I') # possible formats: pysis, MOA, KMT, normal, OR pass a dictionary as below
+e.datasets.append(MulensModel.UlensData(file_name='XXX.dat', format='OGLE', bandpass='I')) # possible formats: pysis, MOA, KMT, normal, OR pass a dictionary as below
 
-e.datasets.append(file_name='other_data.dat', format=dict('hjd': 3, 'err_mag': 8, 'mag': 7)
+e.datasets.append(MulensModel.UlensData(file_name='other_data.dat', format=dict('hjd': 3, 'err_mag': 8, 'mag': 7))
 
