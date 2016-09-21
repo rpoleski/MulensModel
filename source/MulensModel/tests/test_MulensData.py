33f2c26b4d76a53f36ac15662fd0994e63f757c5
jenniferyee <jennifer.c.yee@gmail.com>
2016-09-21 15:55:40 -0400

    Added unit tests for MulensData._get_jd_zeropoint()

--- source/MulensModel/tests/test_MulensData.py
+++ source/MulensModel/tests/test_MulensData.py
@@ -14,3 +14,30 @@ def test_file_read():
     assert data.mag[-1] == 13.913, "magnitude of the last line doesn't match"
 
 
+def test_get_jd_zeropoint_1():
+    test_data = MulensData()
+    assert test_data._get_jd_zeropoint(7500.) == 2450000.
+
+def test_get_jd_zeropoint_2():
+    test_data = MulensData()
+    assert test_data._get_jd_zeropoint(np.array((7500.,7501.))) == 2450000.
+
+def test_get_jd_zeropoint_3():
+    test_data = MulensData()
+    assert test_data._get_jd_zeropoint(np.array((57500.,57501.))) == 2400000.
+
+def test_get_jd_zeropoint_4():
+    test_data = MulensData()
+    assert test_data._get_jd_zeropoint(np.array((9999.,10000.))) == 2450000.
+
+def test_get_jd_zeropoint_5():
+    test_data = MulensData()
+    assert test_data._get_jd_zeropoint(np.array((2457500.,2457501.))) == 0.
+
+def test_get_jd_zeropoint_6():
+    test_data = MulensData()
+    try:
+        assert test_data._get_jd_zeropoint(np.array((np.nan, np.nan))) == 0.
+    except ValueError:
+        assert 1==1
+
