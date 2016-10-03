8331d667c93d53bf569b2e2dcfeebd38af40a44c
jenniferyee <jennifer.c.yee@gmail.com>
2016-10-03 18:29:26 -0400

    Outline of Source class.

---
+++ source/MulensModel/source.py
@@ -0,0 +1,32 @@
+from astropy import units as u
+
+class LimbDarkeningCoeffs(object):
+    pass
+
+class Source(object):
+    def __init__():
+        pass
+
+    @property
+    def distance(self):
+        pass
+
+    @distance.setter
+    def distance(self,new_distance):
+        pass
+
+    @property
+    def angular_size(self):
+        pass
+    
+    @angular_size.setter(self):
+    def angular_size(self,new_size):
+        pass
+
+    @property
+    def limb_darkening(self):
+        pass
+
+    @limb_darkening.setter
+    def limb_darkening(self,new_coeff):
+        pass
