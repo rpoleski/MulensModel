16197746370cc6bb20b30900bed4e03fdbd4e4bd
jenniferyee <jennifer.c.yee@gmail.com>
2016-10-05 17:49:42 -0400

    wrote the basic Source class. Expanded outline of Model class.

--- source/MulensModel/model.py
+++ source/MulensModel/model.py
@@ -1,7 +1,36 @@
+class ModelParameters(object):
+    def __init__(self,t_0=0., u_0=0., t_E=0., rho=None,s=None,
+                 q=None,alpha=None):
+        pass
 
 class Model(object):
-    def __init__(self,parameters=None, lens=None,source=None):
-        pass
+    """
+    Caveats:
+    1. Does not currently have self-consistency checks: e.g. it is
+    possible to define s, a_proj, and a source distance that are not
+    self-consistent. Under these circumstances, the behavior may be
+    unpredictable.
+    2. Does not permit parallax
+    """
+    def __init__(self,parameters=None,
+                 t0=None,u0=None,tE=None,rho=None,s=None,q=None,
+                 alpha=None,
+                 lens=None,source=None,mu_rel=None):
+        """
+        Three ways to define the model:
+        parameters = a ModelParameters() object
+        specify t0, u0, tE (optionally: rho, s, q, alpha)
+        specify physical properties: lens= a Lens() object, 
+            source= a Source() object, mu_rel
+        """
+        if parameters != None:
+            pass
+        elif t0 != None:
+            pass
+        elif source != None:
+            pass
+        else:
+            raise TypeError('Not a valid model definiion')
 
     @property
     def t_0(self):
@@ -15,6 +44,4 @@ class Model(object):
     def t_E(self):
         return self.parameters._t_E
 
-class _Parameters(object):
-    def __init__(self,t_0=0., u_0=0., t_E=0., rho=None):
 
--- source/MulensModel/source.py
+++ source/MulensModel/source.py
@@ -1,32 +1,21 @@
 from astropy import units as u
 
 class LimbDarkeningCoeffs(object):
-    pass
+    def __init__(self,gamma_I=0.44,gamma_V=0.72,gamma_H=0.26):
+        """
+        Default values from Claret probably for a G dwarf.
+        """
+        self.gamma_I = gamma_I
+        self.gamma_V = gamma_V
+        self.gamma_H = gamma_H
+
+default_ld_coeffs = LimbDarkeningCoeffs()
 
 class Source(object):
-    def __init__():
-        pass
+    def __init__(distance=None,angular_size=None,
+                 limb_darkening=None):
+        self.distance=distance
+        self.angular_size=angular_size
+        if limb_darkening == None:
+            self.limb_darkening = default_ld_coeffs
 
-    @property
-    def distance(self):
-        pass
-
-    @distance.setter
-    def distance(self,new_distance):
-        pass
-
-    @property
-    def angular_size(self):
-        pass
-    
-    @angular_size.setter(self):
-    def angular_size(self,new_size):
-        pass
-
-    @property
-    def limb_darkening(self):
-        pass
-
-    @limb_darkening.setter
-    def limb_darkening(self,new_coeff):
-        pass
