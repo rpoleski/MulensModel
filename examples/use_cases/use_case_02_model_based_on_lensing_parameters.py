3f0105301733369ea00888184b220e61a606cd25
jenniferyee <jennifer.c.yee@gmail.com>
2016-10-05 13:04:36 -0400

    Updated use cases 02 and 03 for specifying a model

---
+++ examples/use_cases/use_case_02_model_based_on_lensing_parameters.py
@@ -0,0 +1,14 @@
+from astropy import units as u
+
+import MulensModel
+
+point_lens_2 = MulensModel.Model(t0=7600.,u0=0.1,tE=25.*u.day)
+point_lens_2.plot_lightcurve()
+
+planet_model = MulensModel.Model(t0=7650.,u0=0.3,tE=32.,rho=0.0005,
+                                 s=0.8,q=10.e-3,alpha=32.*u.deg)
+print(planet_model.lens)
+planet_model.source = MulensModel.Source(distance=8.e3*u.pc,
+                                         angular_size=0.001*u.mas)
+print(planet_model.thetaE)
+#Note there is probably a way to get microarcseconds in astropy.units.cds
--- examples/use_cases/use_case_03_model_based_on_physical_parameters.py
+++ examples/use_cases/use_case_03_model_based_on_physical_parameters.py
@@ -3,13 +3,23 @@ from astropy import units as u
 import MulensModel
 
 
+my_lens = MulensModel.Lens(mass=0.5*u.Mass, distance=6.e3*u.pc)
+my_source = MulensModel.Source(distance=8.e3*u.pc)
+
+point_lens = MulensModel.Model(lens=my_lens,source=my_source)
+point_lens.plot_lightcurve() #plots magnification curve over +/-2*tE
+point_lens.source.Imag = 18.
+point_lens.blend.Imag = 20.
+point_lens.plot_lightcurve(range=[-1.,1.])
+
+
 l = MulensModel.Lens(n_components=2)
-l.m_1 = 1.0 * u.M_sun
-l.m_2 = 0.1 * u.M_jup
-l.distance = 2. * u.kpc
+l.mass_1 = 1.0*u.solMass
+l.mass_2 = 0.1*u.jupiterMass
+l.distance = 2.e3*u.pc
 
 s = MulensModel.Source()
-s.distance = 8. * u.kpc
+s.distance = 8.e3*u.pc
 
 m = MulensModel.Model(lens=l, source=s)
 
