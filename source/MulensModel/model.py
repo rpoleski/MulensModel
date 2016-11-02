fd530d7d0c141d6bfe97c8584d4327e1de728326
jenniferyee <jennifer.c.yee@gmail.com>
2016-11-02 17:11:29 -0400

    Added coords to Model class. Works for basic model definition. setting ra and dec after the fact does not work.

--- source/MulensModel/model.py
+++ source/MulensModel/model.py
@@ -1,4 +1,6 @@
 import numpy as np
+from astropy.coordinates import SkyCoord
+from astropy import units as u
 
 from MulensModel.modelparameters import ModelParameters
 
@@ -9,14 +11,14 @@ class Model(object):
     possible to define s, a_proj, and a source distance that are not
     self-consistent. Under these circumstances, the behavior may be
     unpredictable.
-    2. Does not permit parallax
     """
     def __init__(self, parameters=None,
                  t_0=0., u_0=None, t_E=0., rho=None, s=None, q=None,
                  alpha=None,
                  pi_E=None, pi_E_N=None, pi_E_E=None,
                  pi_E_ref=None,
-                 lens=None, source=None, mu_rel=None):
+                 lens=None, source=None, mu_rel=None,
+                 coords=None,ra=None,dec=None):
         """
         Three ways to define the model:
         1. parameters = a ModelParameters() object
@@ -24,6 +26,10 @@ class Model(object):
         3. specify physical properties: lens= a Lens() object, 
             source= a Source() object, mu_rel
         method 3 not implemented.
+
+        When defining event coordinates, may specify coords as an
+        astropy.coordinates.SkyCoord object, otherwise assumes RA is
+        in hour angle and DEC is in degrees.
         """
         self.parameters = ModelParameters(t_0=t_0, u_0=u_0, t_E=t_E)
         if parameters is not None:
@@ -61,6 +67,21 @@ class Model(object):
             pass
         if source is not None:
             pass
+
+        coords_msg = 'Must specify both or neither of ra and dec'
+        if coords is not None:
+            if isinstance(coords,SkyCoord):
+                self._coords = coords
+            else:
+                self._coords = SkyCoord(coords,unit=(u.hourangle,u.deg))
+        if ra is not None:
+            if dec is not None:
+                self._coords = SkyCoord(ra, dec, unit=(u.hourangle,u.deg))
+            else:
+                raise AttributeError(coords_msg)
+        else:
+            if ra is not None:
+                raise AttributeError(coords_msg)
 
         self._magnification = None
 
@@ -172,5 +193,66 @@ class Model(object):
     @source.setter
     def source(self, new_lens):
         pass
-    
+
+    @property
+    def coords(self):
+        """
+        Sky coordinates (RA,Dec)
+        """
+        return self._coords
+
+    @coords.setter
+    def coords(self,new_value):
+        if isinstance(new_value,SkyCoord):
+            self._coords = new_value
+        else:
+            self._coords = SkyCoord(new_value, unit=(u.hourangle,u.deg))
+
+    @property
+    def ra(self):
+        """
+        Right Ascension
+        """
+        return self._coords.ra
+
+    @ra.setter
+    def ra(self, new_value):
+        """
+        Does not work. See __name__=="__main__"
+        """
+        try:
+            self._coords.ra = new_value
+        except AttributeError:
+            self._coords = SkyCoord(new_value,0.0,unit=(u.hourangle,u.deg))
+
+    @property
+    def dec(self):
+        """
+        Declination
+        """
+        return self._coords.dec
+
+    @dec.setter
+    def dec(self, new_value):
+        """
+        Does not work. See __name__=="__main__"
+        """
+        try:
+            self._coords.dec = new_value
+        except AttributeError:
+            self._coords = SkyCoord(0.0,new_value,unit=(u.hourangle,u.deg))
+
+if __name__ == "__main__":
+    model_1 = Model(coords="18:00:00 -30:00:00")
+    print(model_1.coords,model_1.ra,model_1.dec)
+
+    model_2 = Model()
+    model_2.ra = "17:00:00"
+    print(model_2.coords,model_2.ra,model_2.dec)
+    model_2.dec = "40:03:01"
+    print(model_2.coords,model_2.ra,model_2.dec)
+
+    model_3 = Model()
+    model_3.coords = "17:00:00 -27:32:14"
+    print(model_3.coords,model_3.ra,model_3.dec)
 
