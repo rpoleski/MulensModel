a4f6930626ea28934394e8315296d8e1a29264ad
Radek Poleski <radek.poleski@gmail.com>
2016-11-09 15:59:00 -0500

    correction of the previous commit - it was about ModelParameters, not ModelParallax

--- source/MulensModel/tests/test_ModelParameters.py
+++ source/MulensModel/tests/test_ModelParameters.py
@@ -3,7 +3,7 @@ import unittest
 from MulensModel.modelparameters import ModelParameters
 
 
-class TestModelParallax(unittest.TestCase):
+class TestModelParameters(unittest.TestCase):
     def test_too_many_parameters_for_init(self):
         with self.assertRaises(ValueError):
             mp = ModelParameters(pi_E=(1., 1.), pi_E_N=1.)
