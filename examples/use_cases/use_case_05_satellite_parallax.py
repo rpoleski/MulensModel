7deadef3fc829c22b552b85329927310cb62c57e
jenniferyee <jennifer.c.yee@gmail.com>
2016-11-30 15:34:42 -0500

    Expanded Use Case 05 to include coordinates

--- examples/use_cases/use_case_05_satellite_parallax.py
+++ examples/use_cases/use_case_05_satellite_parallax.py
@@ -2,18 +2,30 @@ import numpy as np
 
 import MulensModel
 
+#Basic Example
+example_time = np.arange(7000., 7600., 0.1)
+example_magnitude = np.zeros(example_time.size)
+example_err = np.zeros(example_time.size.size)
 
-t = np.arange(7000., 7600., 0.1)
-q = np.zeros(t.size)
-e = np.zeros(t.size)
+example_dataset = MulensModel.MulensData(
+    np.array(example_time, example_magnitude, example_err), satellite='K2')
 
-d = MulensModel.MulensData(np.array(t, q, e), satellite='K2')
+print(example_dataset.satellite)
 
-print(d.satellite)
-
-m = MulensModel.Model()
-m.parallax(satellite=True, earth_orbital=False, topocentric=False) 
+example_model = MulensModel.Model()
+example_model.parallax(satellite=True, earth_orbital=False, topocentric=False) 
 #default is all True
+
+#Spitzer Example
+spitzer_data = MulensModel.MulensData(
+    file_name="Spitzer_data.dat", date_fmt="HJD", satellite="Spitzer", 
+    ephemrides_file="Spitzer_ephemrides.dat")
+
+model = MulensModel.Model()
+
+spitzer_event = MulensModel.Event(
+    data=spitzer_data, model=model, coords="17:50:00 -29:00:05")
+
 
 
 
