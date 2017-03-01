58a3f5d51fb128ae597af94aed2da50bdf9edf39
jenniferyee <jennifer.c.yee@gmail.com>
2017-03-01 17:24:19 -0500

    Updated Example 01

--- examples/example_01_models.py
+++ examples/example_01_models.py
@@ -15,8 +15,7 @@ pspl = Model(t_0=t_0, u_0=u_0, t_E=t_E)
 #Create a planet model with same PSPL parameters
 s = 1.5
 q = 0.001
-alpha = np.rad2deg(0.37) 
-print('WARNING: This value of alpha does not produce a planet perturbation. JCY.')
+alpha = np.rad2deg(np.pi - 0.37)
 
 planet = Model(t_0=t_0, u_0=u_0, t_E=t_E, s=s, q=q, alpha=alpha)
 
@@ -32,14 +31,22 @@ pspl.plot_lc(f_source=0.5, f_blend=0.5, label='fs=0.5, fb=0.5')
 pl.legend(loc='best')
 pl.title('A Point Lens Model in Magnitudes')
 
-#Plot planet and PSPL models and show difference in magnification at planet perturbation
+#Plot planet and PSPL models and show difference in magnification at
+#planet perturbation
 pl.figure()
-pspl.plot_magnification(color='blue', linestyle=':', zorder=1)
-planet.plot_magnification(color='red',linestyle='-', zorder=2)
+pspl.plot_magnification(
+    color='blue', linestyle=':', zorder=1, label='Point Lens')
+planet.plot_magnification(
+    color='red',linestyle='-', zorder=2, label='Planet')
 t_p = t_0+0.768*t_E
-pl.plot(
-    [t_p, t_p], [pspl.magnification(t_p), planet.magnification(t_p)], 
-    color='black')
 pl.title('Planet vs. Point Lens Models')
+pl.legend(loc='best')
+
+#Plot detail of the planet perturbation
+pl.figure()
+planet.plot_magnification(
+    t_range=[3592, 3593], 
+    color='red',linestyle='-', zorder=2, label='Planet')
+pl.title('Planetary Perturbation Detail')
 
 pl.show()
