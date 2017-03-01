7ab4daa7410f0a57d1bb42b698f6ff95463a20ed
Radek Poleski <radek.poleski@gmail.com>
2017-03-01 13:35:31 -0500

    failing Event test for 2 datasets corrected; ALL tests are passed

--- source/MulensModel/tests/test_Event.py
+++ source/MulensModel/tests/test_Event.py
@@ -50,36 +50,20 @@ def test_event_get_chi2_double_source_simple():
     t_E = 17.94002
     
     data = MulensData(file_name=SAMPLE_FILE_01)
-  
-    t_02 = 5800.
-    u_02 = 0.01
-    t_E2 = 7.0
-    f_s2 = 100.
-    tau = (data.time - t_02) / t_E2
-    u2 = u_02 * u_02 + tau * tau
-    u = u2**.5
-    magnification = (u2 + 2.) / u / (u2 + 4.)**.5
 
     ev = Event()
     mod = Model(t_0=t_0, u_0=u_0, t_E=t_E)
-    mod.set_datasets([data])
+    mod.set_datasets([data, data])
 
-    # Hacking that should not be done by user
-    (data._brightness_input, data._brightness_input_err) = (data.flux+magnification*f_s2, data.err_flux)
-    data.flux = data._brightness_input
-    data.err_flux = data._brightness_input_err
-    data.input_fmt = 'flux'
-    (data.mag, data.err_mag) = Utils.get_mag_and_err_from_flux(data.flux, data.err_flux)
-    mod._magnification = [np.array([mod.magnification[0], magnification.tolist()])]
-    
     ev.model = mod
-    ev.datasets = [data]
+    ev.datasets = [data, data]
 
     chi2 = ev.get_chi2()
     
     assert isinstance(chi2, float), 'wrong type of chi2'
-    assert chi2 > 427. and chi2 < 430., 'wrong valus of chi2 for double source model'
- 
+    message = 'problem in resulting chi2 for 2 exactly the same datasets'
+    np.testing.assert_almost_equal(chi2, 857.17310, decimal=4, err_msg=message)
+
 """
 def test_event_get_chi2():
     #Generate a model
