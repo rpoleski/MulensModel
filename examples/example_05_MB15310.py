faea9f8b9a6f51b643fc1f20b3b4d8dc299338ce
jenniferyee <jennifer.c.yee@gmail.com>
2017-03-09 09:42:09 -0500

    Sketched out Example 05 for displaying MB08310 (multiple datasets)

---
+++ examples/example_05_MB15310.py
@@ -0,0 +1,22 @@
+"""
+Plots data and model for MB15310 with residuals (no fitting). 
+
+From Janczak et al. 2010, ApJ 711, 731
+
+NOTE: residuals have not been implemented in model.py
+"""
+
+#Read in MB15310 data files (see data/MB15310) as MulensData objects
+
+#Define basic point lens model
+#t_0 = 2454656.39975
+#u_0 = 0.00300
+#t_E = 11.14
+#rho = t_star/t_E (t_star = 0.05487) #Not sure rho is implemented
+
+#Combine the data and model into an event
+
+#Plot the data and model
+
+#Plot the residuals (not implemented)
+
