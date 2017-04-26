e31dd2d3be23e21605565746705db6259624b63c
Radek Poleski <radek.poleski@gmail.com>
2017-04-26 18:26:38 -0400

    Caustic unit test corrected

--- source/MulensModel/tests/test_Caustic.py
+++ source/MulensModel/tests/test_Caustic.py
@@ -2,22 +2,24 @@ import numpy as np
 
 import MulensModel
 from MulensModel.caustics import Caustics
-        
+
+
 MODULE_PATH = "/".join(MulensModel.__file__.split("/source")[:-1])
 
 SAMPLE_FILE_01 = MODULE_PATH + "/data/MB11293_caustics.dat"
 
 test_caustics = np.genfromtxt(SAMPLE_FILE_01, names=['X', 'Y'], dtype=None)
 
+
 def test_caustic():
     s = 0.548
     q = 0.0053
 
-    offset_x = s * ( -0.5 + (1. / (1. + q)) )
-    center_of_mass = s * (q / (1. + q))
-
     caustics = Caustics(q=q, s=s)
     caustics.calculate()
 
-    assert caustics._x[100] == test_caustics['X'][100]+offset_x+center_of_mass
-    assert caustics._y[100] == test_caustics['Y'][100]
+    shift_x = s / 2.
+    
+    np.testing.assert_almost_equal(caustics._x[100], test_caustics['X'][100]+shift_x, decimal=3)
+    np.testing.assert_almost_equal(caustics._y[100], test_caustics['Y'][100], decimal=3)
+
