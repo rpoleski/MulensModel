116117a4a35c4cdde1e7838b8559aa83d12a7c99
Radek Poleski <radek.poleski@gmail.com>
2017-05-18 10:09:06 -0400

    rebuilding limbdarkeningcoeffs.py

--- source/MulensModel/limbdarkeningcoeffs.py
+++ source/MulensModel/limbdarkeningcoeffs.py
@@ -1,19 +1,53 @@
 from astropy import units as u
+from MulensModel.utils import Utils
 
 
 class LimbDarkeningCoeffs(object):
     """
-    Linear limb-darkening parameters. Defined as gamma = (2 * u) / (3 - u)
+    Linear limb-darkening parameters. Both gamma and u conventions can be used. 
+    The u convention is more frequently used in studies other in microlensing. 
+    It has fixed flux at the center. An et al. 2002 (ApJ 572, 521,
+    http://adsabs.harvard.edu/abs/2002ApJ...572..521A) introduced gamma convention:
+    
+    gamma = (2 * u) / (3 - u)
+    
+    u = 3 * gamma / (2 + gamma)
+    
+    Note that gamma convention has fixed total flux.
     """
-    # Possibly this needs to be a dictionary or just more
-    # extensible. Think MOA limb-darkening.
-    def __init__(self, gamma_I=0.44, gamma_V=0.72, gamma_H=0.26):
-        """
-        Set the limb-darkening coefficients.
-
-        Default values from Claret probably for a G dwarf.
-        """
-        self.gamma_I = gamma_I
-        self.gamma_V = gamma_V
-        self.gamma_H = gamma_H
-
+    def __init__(self): #, gamma_I=0.44, gamma_V=0.72, gamma_H=0.26):
+        self._gammas_for_band = dict()
+        
+    def set_limb_coef_gamma(self, bandpass, gamma):
+        """set gamma coefficient for given band"""
+        self._gammas_for_band[bandpass] = gamma
+        
+    def set_limb_coef_u(self, bandpass, u):
+        """set u coefficient for given band"""
+        self._gammas_for_band[bandpass] = Utils.u_to_gamma(u)
+    
+    def limb_coef_gamma(self, bandpass):
+        """gives gamma coefficient for given band"""
+        return self._gammas_for_band[bandpass]
+    
+    def limb_coef_u(self, bandpass):
+        """gives u coefficient for given band"""
+        return Utils.gamma_to_u(self._gammas_for_band[bandpass])
+        
+    def weighted_limb_coef_gamma(self, weights):
+        """get weighted limb darkening coefficient
+        weights is a distionary e.g. weights = {'I': 1.5, 'V': 1.} """
+        if not isinstance(weights, dict):
+            raise TypeError("LimbDarkeningCoeffs.weighted_limb_coef_gamma() " +
+                    "parameter has to be dict, not {:}".format(type(weights)))
+        gamma_sum = 0.
+        weight_sum = 0.
+        for (band, weight) in weights.items():
+            try:
+                gamma_sum += self.limb_coef_gamma(band)
+            except KeyError:
+                msg = "The bandpass {:} was not set for limb darkening"
+                raise KeyError(msg.format(band))
+            weight_sum += weight
+        return gamma_sum / weight_sum
+        
