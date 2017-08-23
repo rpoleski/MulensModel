866ed72e22d62235c7a7f921ee92155a20b8915c
jenniferyee <jennifer.c.yee@gmail.com>
2017-08-23 17:00:04 -0400

    Found error in test_mulenssystem. Error has not been resolved.

--- source/MulensModel/tests/test_MulensSystem.py
+++ source/MulensModel/tests/test_MulensSystem.py
@@ -5,16 +5,17 @@ from MulensModel.mulensobjects.mulenssystem import MulensSystem
 from MulensModel.mulensobjects.lens import Lens
 from MulensModel.mulensobjects.source import Source
 
-kappa = 8.14611833456212 * u.mas / u.solMass
-lens = {'mass': 0.3 * u.solMass, 'dist': 4 * 10**3 * u.pc}
-source = {'dist' : 8 * 10**3 * u.pc}
-mu_rel = 3. * u.mas / u.yr
-pi_rel = (lens['dist'].to(u.mas, equivalencies=u.parallax()) 
-          - source['dist'].to(u.mas, equivalencies=u.parallax()))
-thetaE = np.sqrt( kappa * lens['mass'] * pi_rel)
-tE = thetaE / mu_rel
 
 def test_mulenssystem():
+    kappa = 8.144183118384794 * u.mas / u.solMass
+    lens = {'mass': 0.3 * u.solMass, 'dist': 4 * 10**3 * u.pc}
+    source = {'dist' : 8 * 10**3 * u.pc}
+    mu_rel = 3. * u.mas / u.yr
+    pi_rel = (lens['dist'].to(u.mas, equivalencies=u.parallax()) 
+              - source['dist'].to(u.mas, equivalencies=u.parallax()))
+    thetaE = np.sqrt( kappa * lens['mass'] * pi_rel)
+    tE = thetaE / mu_rel
+
     test_system = MulensSystem(
         lens=Lens(mass=lens['mass'], distance=lens['dist']), 
         source=Source(distance=source['dist']),
@@ -25,10 +26,16 @@ def test_mulenssystem():
     assert test_system.t_E == tE.to(u.day)
 
 def test_mulenssytem():
+    #This test fails due to a discrepancy in the value of kappa (see
+    #above value) and kappa as calculated from astropy constants in
+    #MulensSystem. Note: 8/23 changed the value of kappa used here and
+    #updated theta_E but not subsequent values (test still fails for
+    #thetaE).
+
     lens = Lens(mass=0.64, distance=4.0)
     source = Source(distance=8.0)
     system = MulensSystem(lens=lens, source=source)
 
-    np.testing.assert_almost_equal(system.theta_E.value, 0.80727, decimal=5)
+    np.testing.assert_almost_equal(system.theta_E.value, 0.807177, decimal=5)
     np.testing.assert_almost_equal(system.r_E.value, 3.22909, decimal=5)
     np.testing.assert_almost_equal(system.r_E_tilde.value, 6.45818, decimal=5)
