3c465f37d3da8a5949350dcd352485f641b063de
jennyee <jennifer.c.yee@gmail.com>
2017-09-06 16:43:07 -0400

    Some edits to UC 20 for Orbital Motion.

--- examples/use_cases/use_case_20_lens_orbital_motion.py
+++ examples/use_cases/use_case_20_lens_orbital_motion.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 
 import MulensModel
 
+"""
+This use case needs a description.
+"""
 
 # point lens parameters:
 t_0 = 2457123.456
@@ -21,18 +24,22 @@ ds_dt = 0.5 / u.year
 model = MulensModel.Model()
 model.set_parameters(t_0=t_0, u_0=u_0, t_E=t_E, rho=rho, q=q, 
                     alpha=alpha, dalpha_dt=dalpha_dt, 
-                    s=s, ds_dt=ds_dt)
+                    s=s, ds_dt=ds_dt, t_orb=t_0)
 # QUESTION - should we have alpha_0 and s_0 instead of alpha and s?
 
 # Get the values of parameters:
 try:
     print(model.s) # Yes, this is not allowed. Same for alpha.
-except:
+except: #this exception should be thrown from inside model.s. It should not be part of the use case. JCY
     print("You cannot call Model.s because lens orbital was specified " +
             "try Model.s_0 or Model.s_for_t(t))")
+#OR
+print(model.s) # should return input s value ( i.e. s_0 = s(t_orb) )
+#RP and JCY disagree on the implementation
+
 dt = 36.525 # This is in days.
-print(model.s_for_t(t=t_0+dt)) # should return 1.55
-print(model.alpha_for_t(t=t_0-dt)) # should return 7.345 u.deg
+print(model.s_for_epoch(t_0+dt)) # should return 1.55
+print(model.alpha_for_epoch(epoch=t_0-dt)) # should return 7.345 u.deg
 
 # Print projected orbital velocity
 print(model.gamma_parallel) # should return 0.3333333 1/u.year
@@ -43,6 +50,7 @@ print(model.gamma) # should return 0.9346 1/u.year
 # Make a nice plot
 plt.figure()
 model.plot_caustics()
+model.plot_caustics(epoch=t_0+dt)
 model.plot_trajectory()
 plt.title('This plot shows a nice curved trajectory')
 plt.show()
