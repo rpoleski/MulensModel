292bed74622b6ed83663d41b36c0bd482829b8e7
Radek Poleski <radek.poleski@gmail.com>
2017-09-28 16:57:02 -0400

    enabling math in rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,8 @@
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
-    'sphinx.ext.ifconfig']
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
