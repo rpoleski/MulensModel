15ecb78b580b04bf570b56b448c4c3b89044f318
jenniferyee <jennifer.c.yee@gmail.com>
2017-10-12 15:01:08 -0400

    self.bad is defined as all False by default.

--- source/MulensModel/mulensdata.py
+++ source/MulensModel/mulensdata.py
@@ -118,6 +118,8 @@ class MulensData(object):
 
         if bad is not None:
             self.bad = bad
+        else:
+            self.bad = np.zeros(len(self.time), dtype=bool)
         
         #Set up satellite properties (if applicable)
         self.ephemerides_file = ephemerides_file
