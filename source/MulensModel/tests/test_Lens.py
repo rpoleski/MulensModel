2f396adfc2256e73d182e65c797839a63dabc7b0
Radek Poleski <radek.poleski@gmail.com>
2017-10-20 16:19:33 -0400

    removing old tests for almost equal

--- source/MulensModel/tests/test_Lens.py
+++ source/MulensModel/tests/test_Lens.py
@@ -40,33 +40,31 @@ def test_q_total_mass():
     np.testing.assert_almost_equal(lens.mass_1.value, 0.64)
     np.testing.assert_almost_equal(lens.mass_2.value, 0.16)
 
-class TestQRoutines(unittest.TestCase):
-    def test_q_success(self):
-        lens = Lens()
-        lens.mass_1 = 1.0*u.solMass
-        lens.q = 10.**3
-        assert lens.mass_2 == 10.**3*u.solMass
-        assert lens.total_mass == (1.0+10.**3)*u.solMass
+def test_q_success():
+    lens = Lens()
+    lens.mass_1 = 1.0*u.solMass
+    lens.q = 10.**3
+    assert lens.mass_2 == 10.**3*u.solMass
+    assert lens.total_mass == (1.0+10.**3)*u.solMass
         
-class TestInitRoutines(unittest.TestCase):
-    def test_init_success(self):
-        lens_1 = Lens(mass_1=1.8*u.solMass, mass_2=3.*u.solMass)
-        self.assertAlmostEqual(lens_1.mass_1.value, 1.8, places=7)
-        self.assertAlmostEqual(lens_1.mass_2.value, 3., places=7)
-        
-        lens_2 = Lens(mass=1.8*u.solMass, distance=3.*10.**3*u.pc)
-        self.assertAlmostEqual(lens_2.mass.value, 1.8, places=7)
-        self.assertAlmostEqual(lens_2.mass_1.value, 1.8, places=7)
-        self.assertAlmostEqual(lens_2.distance.value, 3.*10.**3,places=7)
-        
-        lens_3 = Lens(mass_1=1.0*u.solMass, q=0.1,s=0.9)
-        self.assertAlmostEqual(lens_3.mass_1.value, 1.0, places=7)
-        self.assertAlmostEqual(lens_3.mass_2.value, 0.1, places=7)
-        assert lens_3.s == 0.9
+def test_init_success():
+    lens_1 = Lens(mass_1=1.8*u.solMass, mass_2=3.*u.solMass)
+    np.testing.assert_almost_equal(lens_1.mass_1.value, 1.8)
+    np.testing.assert_almost_equal(lens_1.mass_2.value, 3.)
+    
+    lens_2 = Lens(mass=1.8*u.solMass, distance=3.*10.**3*u.pc)
+    np.testing.assert_almost_equal(lens_2.mass.value, 1.8)
+    np.testing.assert_almost_equal(lens_2.mass_1.value, 1.8)
+    np.testing.assert_almost_equal(lens_2.distance.value, 3.*10.**3)
+    
+    lens_3 = Lens(mass_1=1.0*u.solMass, q=0.1,s=0.9)
+    np.testing.assert_almost_equal(lens_3.mass_1.value, 1.0)
+    np.testing.assert_almost_equal(lens_3.mass_2.value, 0.1)
+    assert lens_3.s == 0.9
 
-        lens_4 = Lens(s=1.0, q=0.1)
-        assert lens_4.q == 0.1
-        assert lens_4.s == 1.0
+    lens_4 = Lens(s=1.0, q=0.1)
+    assert lens_4.q == 0.1
+    assert lens_4.s == 1.0
 
 def test_a_proj_success():
     lens = Lens(mass_1=1.0*u.solMass,mass_2=0.1*u.solMass,a_proj=1.0*u.au,
@@ -74,21 +72,20 @@ def test_a_proj_success():
     assert lens.total_mass == 1.1*u.solMass
     assert lens.q == 0.1
 
-class TestEpsilonRoutines(unittest.TestCase):
-    def test_3_body_success(self):
-        lens_1 = Lens(total_mass=1.3*u.solMass,q=[0.1,0.2],s=[0.1,0.3])
-        
-        self.assertAlmostEqual(lens_1.mass_1.value, 1., places=7)
-        self.assertAlmostEqual(lens_1.mass_2.value, 0.1, places=7)
-        self.assertAlmostEqual(lens_1.mass_3.value, 0.2, places=7)
+def test_3_body_success():
+    lens_1 = Lens(total_mass=1.3*u.solMass,q=[0.1,0.2],s=[0.1,0.3])
+    
+    np.testing.assert_almost_equal(lens_1.mass_1.value, 1.)
+    np.testing.assert_almost_equal(lens_1.mass_2.value, 0.1)
+    np.testing.assert_almost_equal(lens_1.mass_3.value, 0.2)
 
-        lens_2 = Lens()
-        lens_2.mass_1 = 0.7*u.solMass
-        lens_2.mass_2 = 0.1*u.solMass
-        lens_2.mass_3 = 0.2*u.solMass
-        
-        self.assertAlmostEqual(lens_2.mass_1.value, 0.7, places=7)
-        self.assertAlmostEqual(lens_2.mass_2.value, 0.1, places=7)
-        self.assertAlmostEqual(lens_2.mass_3.value, 0.2, places=7)
-        self.assertAlmostEqual(lens_2.total_mass.value, 1., places=7)
+    lens_2 = Lens()
+    lens_2.mass_1 = 0.7*u.solMass
+    lens_2.mass_2 = 0.1*u.solMass
+    lens_2.mass_3 = 0.2*u.solMass
+    
+    np.testing.assert_almost_equal(lens_2.mass_1.value, 0.7)
+    np.testing.assert_almost_equal(lens_2.mass_2.value, 0.1)
+    np.testing.assert_almost_equal(lens_2.mass_3.value, 0.2)
+    np.testing.assert_almost_equal(lens_2.total_mass.value, 1.)
 
