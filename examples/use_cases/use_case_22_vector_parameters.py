b64393703ccab22ca1643f8dd66d4228628e8ce7
Jennifer Yee <jyee@SI381826.local>
2017-11-20 17:57:42 -0500

    Updated with new ModelParameters format. Untested due to NotImplemented.

--- examples/use_cases/use_case_22_vector_parameters.py
+++ examples/use_cases/use_case_22_vector_parameters.py
@@ -1,5 +1,10 @@
 import MulensModel
 
+raise NotImplementedError('ModelParameters.vector not implemented')
+
+# JCY: I don't see the point of returning a vector. I also think it
+# creates a risk for the user: there is no way to identify which value
+# goes with which variable, you have to know this in advance.
 
 # Show a new way of accessing model parameters.
 t_0 = 2456789.0123
@@ -9,11 +14,20 @@ rho = 0.001
 pi_E_N = 0.1
 pi_E_E = 0.2
 
-parameters = MulensModel.ModelParameters(t_0=t_0, u_0=u_0, t_E=t_E, 
-                pi_E_N=pi_E_N, pi_E_E=pi_E_E)
+parameters = MulensModel.ModelParameters(
+    {'t_0': t_0, 'u_0': u_0, 't_E': t_E, 
+     'pi_E_N': pi_E_N, 'pi_E_E': pi_E_E)
 print(parameters.vector)
 # returns np.array([t_0, u_0, t_E, pi_E_N, pi_E_E])
 
+# Below is non-functional:
+# A new variable cannot be introduced after the model has been defined.
+#
+# JCY: This exactly illustrates the confusion I'm worried
+# about. Why is rho introduced in the middle of the vector? This is an
+# arbitrary convention commonly used by microlensers, but there is no
+# fundamental reason why rho should come after t_E rather than after
+# parallax. It is more transparent just to print (and access) the dictionary.
 parameters.rho = rho
 print(parameters.vector)
 # returns np.array([t_0, u_0, t_E, rho, pi_E_N, pi_E_E])
