ea673a1378409414436e434430619bf7a7925c77
Jennifer Yee <jyee@SI381826.local>
2017-11-20 09:39:07 -0500

    Updated for new ModelParameters. DNW - MulensData problem.

--- examples/use_cases/use_case_05_satellite_parallax.py
+++ examples/use_cases/use_case_05_satellite_parallax.py
@@ -10,11 +10,12 @@ import MulensModel
 file_dir = os.path.join(MulensModel.MODULE_PATH, "data")
 spitzer_data = MulensModel.MulensData(
     file_name=os.path.join(file_dir, "ob151100_Spitzer_ref_v1.dat"), 
-    satellite="Spitzer", 
+    satellite="Spitzer", #this keyword does not work.
     ephemrides_file=os.path.join(file_dir, "Spitzer_ephemrides_01.dat"))
 
 #Create Model
-model = MulensModel.Model()
+model = MulensModel.Model(
+    {'t_0': 0., 'u_0': 0.1, 't_E': 1.})
 print('Default Parallax Settings:')
 print(model.parallax())
 model.parallax(satellite=True, earth_orbital=False, topocentric=False)
