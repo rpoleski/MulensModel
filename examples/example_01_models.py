Fixed rotation, but not shift_x.

examples/example_01_models.py
@@ -22,10 +22,13 @@ pspl = Model({'t_0': t_0, 'u_0': u_0, 't_E': t_E})
 #Create a planet model with same PSPL parameters
 s = 1.5
 q = 0.001
-alpha = np.rad2deg(np.pi - 0.37)
+alpha = np.rad2deg(2.756 - np.pi)
+rho = 0.001
 
 planet = Model(
-    {'t_0': t_0, 'u_0': u_0, 't_E': t_E, 's': s, 'q': q, 'alpha': alpha})
+    {'t_0': t_0, 'u_0': u_0, 't_E': t_E, 's': s, 'q': q, 'alpha': alpha, 
+     'rho': rho})
+planet.set_magnification_methods([3589., 'VBBL', 3595.])
 
 #Plot PSPL model
 pl.figure()
@@ -56,8 +59,11 @@ planet.plot_magnification(t_range=[3592, 3593],
 pl.title('Planetary Perturbation Detail')
 
 #Plot source trajectory and caustic
-pl.figure()
-planet.plot_trajectory(caustics=True)
+pl.figure(figsize=(6,6))
+planet.plot_trajectory(t_range=[t_0 - t_E, t_0], caustics=True, color='red')
+planet.plot_trajectory(t_range=[t_0, t_0 + t_E], caustics=True, color='blue')
+pl.xlim(-0.25, 1.0)
+pl.ylim(-0.25, 1.0)
 pl.title('Source Trajectory')
 
 pl.show()

source/MulensModel/tests/test_Model.py
@@ -101,8 +101,8 @@ def test_limb_darkening():
 def test_BLPS_01():
     """simple binary lens with point source"""
     params = ModelParameters({
-            't_0':6141.593, 'u_0':0.5425, 't_E':62.63*u.day, 
-            'alpha':49.58*u.deg, 's':1.3500, 'q':0.00578})
+            't_0': 6141.593, 'u_0': 0.5425, 't_E': 62.63*u.day, 
+            'alpha': 229.58*u.deg, 's': 1.3500, 'q': 0.00578})
     model = Model(parameters=params)
     t = np.array([6112.5])
     data = MulensData(data_list=[t, t*0.+16., t*0.+0.01])
@@ -112,10 +112,12 @@ def test_BLPS_01():
     # np.testing.assert_almost_equal(m, 4.710563917) # This value comes from Andy's getbinp().
 
 def test_BLPS_02():
-    """simple binary lens with extended source and different methods to evaluate magnification"""
+    """simple binary lens with extended source and different methods to
+    evaluate magnification"""
+
     params = ModelParameters({
-            't_0':2456141.593, 'u_0':0.5425, 't_E':62.63*u.day, 'alpha':49.58*u.deg, 
-            's':1.3500, 'q':0.00578, 'rho':0.01})
+            't_0': 2456141.593, 'u_0': 0.5425, 't_E': 62.63*u.day, 
+            'alpha': 229.58*u.deg, 's': 1.3500, 'q': 0.00578, 'rho': 0.01})
     model = Model(parameters=params)
     
     t = (np.array([6112.5, 6113., 6114., 6115., 6116., 6117., 6118., 6119]) + 
@@ -143,8 +145,8 @@ def test_BLPS_02_AC():
     - version with adaptivecontouring
     """
     params = ModelParameters({
-            't_0':2456141.593, 'u_0':0.5425, 't_E':62.63*u.day, 'alpha':49.58*u.deg, 
-            's':1.3500, 'q':0.00578, 'rho':0.01})
+            't_0': 2456141.593, 'u_0': 0.5425, 't_E': 62.63*u.day, 
+            'alpha': 229.58*u.deg, 's': 1.3500, 'q': 0.00578, 'rho': 0.01})
     model = Model(parameters=params)
     
     t = (np.array([6112.5, 6113., 6114., 6115., 6116., 6117., 6118., 6119]) + 
@@ -175,8 +177,8 @@ def test_BLPS_02_AC():
 def test_methods_parameters():
     """make sure additional parameters are properly passed to very inner functions"""
     params = ModelParameters({
-            't_0':2456141.593, 'u_0':0.5425, 't_E':62.63*u.day, 'alpha':49.58*u.deg, 
-            's':1.3500, 'q':0.00578, 'rho':0.01})
+            't_0': 2456141.593, 'u_0': 0.5425, 't_E': 62.63*u.day, 
+            'alpha': 229.58*u.deg, 's': 1.3500, 'q': 0.00578, 'rho': 0.01})
     model = Model(parameters=params)
     
     t = np.array([2456117.])

source/MulensModel/trajectory.py
@@ -16,7 +16,8 @@ class Trajectory(object):
     microlensing parallax affects the trajectory.
 
     For binary lens, the origin of the coordinate system is at the
-    center of mass with higher mass at negative X and Y=0.
+    center of mass with higher mass (assuming q < 1) at negative X and
+    Y=0.
 
     Arguments :
         times: [*float*, *list*, *np.ndarray*], required
@@ -125,9 +126,8 @@ class Trajectory(object):
             cos_alpha = np.cos(self.parameters.alpha).value
             shift_x = - (self.parameters.s * self.parameters.q /
                          (1. + self.parameters.q))
-            vector_x = (vector_u * sin_alpha - vector_tau * cos_alpha + 
-                        shift_x)
-            vector_y = -vector_u * cos_alpha - vector_tau * sin_alpha
+            vector_x = vector_tau * cos_alpha - vector_u * sin_alpha + shift_x
+            vector_y = vector_tau * sin_alpha + vector_u * cos_alpha
             # The above equations use alpha in counterclockwise
             # convention, i.e., the same as proposed by Skowron et
             # al. (2011)