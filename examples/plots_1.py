58041bef58db22a3bcfd351b0e6e6b8352e477b3
Radek Poleski <radek.poleski@gmail.com>
2018-01-16 14:22:44 -0500

    adding example with plots for paper

---
+++ examples/plots_1.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import os
+
+from MulensModel.model import Model, SatelliteSkyCoord
+import MulensModel
+
+
+# Define model parameters.
+params = {'t_0': 2456900, 'u_0': 0.2, 't_E': 50.}
+params_pi_E = {'pi_E_N': 0.35, 'pi_E_E': 0.5}
+params_planet = {'rho': 0.002, 's': 1.5, 'q': 0.001, 'alpha': 348.1}
+ra_dec = "18:00:00.00 -28:30:00.0"
+
+# Set models and satellite settings
+model_pspl = Model(params)
+model_planet = Model({**params, **params_planet})
+model_planet.set_magnification_methods([245637, 'VBBL', 2456945])
+model_parallax = Model({**params, **params_pi_E}, coords=ra_dec)
+satellite = SatelliteSkyCoord(os.path.join(MulensModel.MODULE_PATH, 'data',
+    'Spitzer_ephemeris_01.dat'))
+
+# Plot the magnification curves.
+plot_kwargs = {'subtract_2450000': True, 'lw': 2.}
+model_planet.plot_magnification(label="planetary", **plot_kwargs)
+model_pspl.plot_magnification(label="PSPL", linestyle='--', **plot_kwargs)
+model_parallax.parallax(earth_orbital=True)
+model_parallax.plot_magnification(label="annual parallax", linestyle='-.', 
+    **plot_kwargs)
+model_parallax.parallax(satellite=True)
+model_parallax.plot_magnification(label="satellite parallax", 
+    satellite_skycoord=satellite, **plot_kwargs)
+
+plt.legend(loc='best')
+plt.savefig('figure_1.png')
