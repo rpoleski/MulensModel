b757b646428592d151fcfe9932c6edd61a142e90
Radek Poleski <radek.poleski@gmail.com>
2018-02-03 23:24:33 -0500

    significant changes to PointLens tests

--- source/MulensModel/tests/test_PointLens.py
+++ source/MulensModel/tests/test_PointLens.py
@@ -1,11 +1,11 @@
 import numpy as np
 import os
 
-import MulensModel
-from MulensModel import PointLens, ModelParameters
+from MulensModel import PointLens, ModelParameters, MODULE_PATH
 
-DATA_PATH = os.path.join(MulensModel.MODULE_PATH, 'data')
-SAMPLE_FILE = os.path.join(DATA_PATH, 'FSPL_test_1.dat')
+
+SAMPLE_FILE = os.path.join(MODULE_PATH, 'data', 'FSPL_test_1.dat')
+
 
 def get_file_params(filename):
     """Read in the model parameters used to create the file"""
@@ -18,46 +18,46 @@ def get_file_params(filename):
              't_E': float(ulens_params[3]), 'rho': float(ulens_params[4])}),
         float(ulens_params[5]))
 
-DATA = np.genfromtxt(
-    SAMPLE_FILE, dtype=None, 
-    names=['Time', 'b_0', 'b_1', 'Mag_FS', 'Mag_LD', 'Mag'])
-(PARAMETERS, GAMMA) = get_file_params(SAMPLE_FILE)
+def get_variables():
+    """return a few variables used by 4 test functions below"""
+    if 'out' not in get_variables.__dict__:
+        names = ['Time', 'b_0', 'b_1', 'Mag_FS', 'Mag_LD', 'Mag']
+        data = np.genfromtxt(SAMPLE_FILE, names=names)
+        (parameters, gamma) = get_file_params(SAMPLE_FILE)
 
-point_lens = PointLens(parameters=PARAMETERS)
-tau = (DATA['Time'] - PARAMETERS.t_0) / PARAMETERS.t_E
-u = np.sqrt(PARAMETERS.u_0**2 + tau**2)
-z = u / PARAMETERS.rho
-pspl_magnification = (u**2 + 2.) / (u * np.sqrt(u**2 + 4.))
+        point_lens = PointLens(parameters=parameters)
+        tau = (data['Time'] - parameters.t_0) / parameters.t_E
+        u = np.sqrt(parameters.u_0**2 + tau**2)
+        z = u / parameters.rho
+        pspl_magnification = (u**2 + 2.) / (u * np.sqrt(u**2 + 4.))
 
-mid_index = 0
+        get_variables.out = (data, gamma, point_lens, u, z, pspl_magnification)
+    return get_variables.out
+
 
 def test_B_0_function():
+    """test private _B_0_function"""
+    (data, _, point_lens, _, z, _) = get_variables()
     test_b_0 = point_lens._B_0_function(z)
-    np.testing.assert_almost_equal(test_b_0, DATA['b_0'], decimal=5)
+    np.testing.assert_almost_equal(test_b_0, data['b_0'], decimal=5)
 
 def test_B_1_function():
-    print(PARAMETERS)
+    """test private _B_1_function"""
+    (data, _, point_lens, _, z, _) = get_variables()
     test_b_1 = point_lens._B_1_function(z)
-    print(
-        test_b_1[mid_index], 
-        DATA['b_1'][mid_index])
-    np.testing.assert_almost_equal(test_b_1, DATA['b_1'], decimal=5)
+    np.testing.assert_almost_equal(test_b_1, data['b_1'], decimal=4.5)
 
 def test_get_point_lens_finite_source_magnification():
+    """test PLFS"""
+    (data, _, point_lens, u, _, pspl_magnification) = get_variables()
     test_FSPL = point_lens.get_point_lens_finite_source_magnification(
         u, pspl_magnification)
-    print(
-        test_FSPL[mid_index], 
-        DATA['Mag_FS'][mid_index])
-    np.testing.assert_almost_equal(test_FSPL, DATA['Mag_FS'], decimal=5)
+    np.testing.assert_almost_equal(test_FSPL, data['Mag_FS'], decimal=5)
 
 def test_get_point_lens_limb_darkening_magnification():
-    print('gamma = ', GAMMA)
+    """test PLFS+LD"""
+    (data, gamma, point_lens, u, _, pspl_magnification) = get_variables()
     test_FSPL_LD = point_lens.get_point_lens_limb_darkening_magnification(
-        u, pspl_magnification, GAMMA)
-    print(
-        test_FSPL_LD[mid_index], 
-        DATA['Mag_LD'][mid_index])
-    np.testing.assert_almost_equal(test_FSPL_LD, DATA['Mag_LD'], decimal=5)
-
+        u, pspl_magnification, gamma)
+    np.testing.assert_almost_equal(test_FSPL_LD/data['Mag_LD'], 1., decimal=4.5)
 
