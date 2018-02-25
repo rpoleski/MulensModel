e8fadfeb84aa5d56698cf1b4d8b9ff4d2a3ce95d
Radek Poleski <radek.poleski@gmail.com>
2018-02-25 00:39:40 -0500

    minor changes in plots_3

--- examples/plots_3.py
+++ examples/plots_3.py
@@ -19,9 +19,10 @@ OGLE_data = MulensData(
 MOA_data = MulensData(file_name=os.path.join(data_dir, 'OB03235_MOA.tbl.txt'),
     comments=['\\', '|'], phot_fmt='flux')
 
-# Define a model with a 2-body lens:
-model_1S2L = Model({'t_0': 2452848.253135, 'u_0': 0.136012, 't_E': 61.5, 
-    'rho': 0.00096, 'q': 0.0039, 's': 1.120, 'alpha': 223.8})
+# Define a model with a 2-body lens 
+# (these parameters slightly differ from Bond et al. 2004):
+model_1S2L = Model({'t_0': 2452848.06, 'u_0': 0.1317, 't_E': 61.5, 
+    'rho': 0.00096, 'q': 0.0039, 's': 1.120, 'alpha': 223.72})
 
 # Since rho is set, define a time range and method to apply 
 # finite source effects:
@@ -39,6 +40,6 @@ my_event.plot_model(subtract_2450000=True, t_range=t_range, n_epochs=4000,
 
 pyplot.legend(loc='best')
 pyplot.xlim(t_range[0]-2450000., t_range[1]-2450000.)
-pyplot.ylim(19., 16.7)
+pyplot.ylim(19.0, 16.7)
 pyplot.savefig('figure_3.png')
 
