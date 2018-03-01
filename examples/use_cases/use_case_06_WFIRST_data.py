548a4200c3172bc41326a245518d6a5285afa02c
Radek Poleski <radek.poleski@gmail.com>
2018-03-01 00:48:22 -0500

    temporarily removing files for v1.0.0

--- examples/use_cases/use_case_06_WFIRST_data.py
+++
@@ -1,9 +0,0 @@
-import MulensModel
-
-
-raise NotImplementedError('satellite keyword for MulensData not supported')
-
-wfirst_data = MulensModel.MulensData(
-    file_name='interesting_event.dat', satellite='WFIRST') 
-print(wfirst_data.bandpass)
-#automatically sets bandpass='W146'
