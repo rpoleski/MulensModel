cb167dc6ee43fde70f21320143d9e39ef31fb9f0
Radek Poleski <radek.poleski@gmail.com>
2018-05-09 16:54:02 -0400

    going back to dict() in UC26b

--- examples/use_cases/use_case_26b_binary_source_model.py
+++ examples/use_cases/use_case_26b_binary_source_model.py
@@ -28,8 +28,8 @@ times = np.union1d(
     np.arange(t_0_2 - dt_2, t_0_2 + dt_2, 2. * dt_2 / 100.))
 
 # Distinct Models for the two sources.
-model_1 = mm.Model({'t_0': t_0_1, 'u_0': u_0_1, 't_E': t_E))
-model_2 = mm.Model({'t_0': t_0_2, 'u_0': u_0_2, 't_E': t_E))
+model_1 = mm.Model(dict(t_0=t_0_1, u_0=u_0_1, t_E=t_E))
+model_2 = mm.Model(dict(t_0=t_0_2, u_0=u_0_2, t_E=t_E))
 
 mag_1 = model_1.magnification(times)
 mag_2 = model_2.magnification(times)
@@ -37,9 +37,9 @@ combined_model_lc = mm.Utils.get_mag_from_flux(
     f_source_1 * mag_1 + f_source_2 * mag_2 + f_blend)
 
 # Binary Source Model version
-binary_source_model = mm.Model({
-   't_0_1': t_0_1, 'u_0_1': u_0_1, 't_0_2': t_0_2, 'u_0_2': u_0_2, 
-   't_E': t_E, 'flux_ratio': f_source_2/f_source_1})
+binary_source_model = mm.Model(dict(
+    t_0_1=t_0_1, u_0_1=u_0_1, t_0_2=t_0_2, u_0_2=u_0_2, t_E=t_E, 
+    flux_ratio=f_source_2/f_source_1))
 
 # Plots
 pl.figure()
