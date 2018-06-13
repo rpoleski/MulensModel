2677a38c62e22f06b99a745e86f45a68fd43cc92
Radek Poleski <radek.poleski@gmail.com>
2018-06-13 12:07:57 -0400

    first version of setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MulensModel',
+    version='1.2.10',
+    url='git@github.com:rpoleski/MulensModel.git',
+    author='Radek Poleski',
+    author_email='poleski.1@osu.edu',
+    description='packge for modeling gravitational microlensing events',
+    packages=find_packages(),
+    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+)
+
