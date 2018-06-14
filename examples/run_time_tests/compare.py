2ca552bf08b3aee656f6474cf8642399e66a2528
Radek Poleski <radek.poleski@gmail.com>
2018-06-14 19:19:51 -0400

    pyLIMA vs. MM parallax comparison

---
+++ examples/run_time_tests/compare.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+
+sys.path.append("/usr/custom/pyLIMA-1.0.0") 
+from pyLIMA import event, telescopes, microlmodels
+import MulensModel as mm
+
+
+# Common settings:
+t_0 = 2456900.
+u_0 = -0.1
+t_E = 150.
+pi_E_N = 1.
+pi_E_E = -2.
+ra_deg = 270.
+dec_deg = -30.
+time = np.linspace(t_0-80., t_0+80., 1000)
+d_time = 2450000.
+
+# pyLIMA calculations and plots:
+your_event = event.Event()
+your_event.ra = ra_deg
+your_event.dec = dec_deg
+data_1 = np.array([time, time*0.+15., time*0.+.01]).T
+telescope_1 = telescopes.Telescope(light_curve_magnitude=data_1)
+your_event.telescopes.append(telescope_1)
+your_event.check_event()
+model_1 = microlmodels.create_model('PSPL', your_event, parallax=['Annual', t_0])
+model_1.define_model_parameters()
+pyLIMA_parameters = model_1.compute_pyLIMA_parameters([t_0, u_0, t_E, pi_E_N, pi_E_E])
+model = model_1.compute_the_microlensing_model(telescope_1, pyLIMA_parameters)  
+mag = model_1.model_magnification(telescope_1, pyLIMA_parameters)
+plt.plot(time-d_time, mag, 'b.', label='pyLIMA')
+
+# This are the changes I have to make to make the things as close as possible:
+u_0 = -u_0
+pi_E_E = -pi_E_E
+
+# MulensModel calculations and plot:
+coords = SkyCoord(ra_deg, dec_deg, unit=u.deg)
+params = {'t_0': t_0, 'u_0': u_0, 't_E': t_E, 'pi_E_N': pi_E_N, 'pi_E_E': pi_E_E}
+mm_model = mm.Model(params, coords=coords)
+mm_mag = mm_model.magnification(time)
+plt.plot(time-d_time, mm_mag, 'r.', label='MulensModel')
+
+# This is the end:
+plt.legend(loc='best')
+plt.xlabel('JD-2450000')
+plt.ylabel('magnification')
+plt.show()
