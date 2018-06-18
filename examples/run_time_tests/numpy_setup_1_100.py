correcting numerical values

examples/run_time_tests/numpy_setup_1_100.py
@@ -10,6 +10,6 @@ time = data_1[0]
 obs_flux = np.power(10., -0.4 * (data_1[1] - MAG_ZEROPOINT))
 obs_flux_err = data_1[2] * obs_flux * 0.4 * np.log(10.)
 
-t_0 = 5379.57091
-u_0 = 0.52298
-t_E = 17.94002
+t_0 = 2456900.
+u_0 = 0.01
+t_E = 20.
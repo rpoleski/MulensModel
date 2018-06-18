3f350ddac65592c09038e63439e48b2db46957ba
Radek Poleski <radek.poleski@gmail.com>
2018-06-18 17:10:15 -0400

    correcting numerical values

--- examples/run_time_tests/MM_setup_1_1000.py
+++ examples/run_time_tests/MM_setup_1_1000.py
@@ -7,8 +7,8 @@ import MulensModel as mm
 file_1 = "test_1000.txt"
 data_1 = mm.MulensData(file_name=file_1)
 data_1.input_fmt = 'flux'
-t_0 = 5379.57091
-u_0 = 0.52298
-t_E = 17.94002
+t_0 = 2456900.
+u_0 = 0.01
+t_E = 20.
 model = mm.Model({'t_0': t_0, 'u_0': u_0, 't_E': t_E})
 event = mm.Event(datasets=[data_1], model=model)
--- examples/run_time_tests/numpy_setup_1_100.py
+++ examples/run_time_tests/numpy_setup_1_100.py
@@ -10,6 +10,6 @@ time = data_1[0]
 obs_flux = np.power(10., -0.4 * (data_1[1] - MAG_ZEROPOINT))
 obs_flux_err = data_1[2] * obs_flux * 0.4 * np.log(10.)
 
-t_0 = 5379.57091
-u_0 = 0.52298
-t_E = 17.94002
+t_0 = 2456900.
+u_0 = 0.01
+t_E = 20.
