f39012b177766aaccd96299e5978710541d42cb1
Radek Poleski <radek.poleski@gmail.com>
2018-06-18 17:09:24 -0400

    trying to make numpy_chi2_v1() faster

--- examples/run_time_tests/numpy_functions.py
+++ examples/run_time_tests/numpy_functions.py
@@ -1,10 +1,9 @@
-import numpy as np
+from numpy import sqrt, square, reciprocal, sum, polyfit
 
 def numpy_chi2_v1(time, obs_flux, obs_flux_err, t_0, u_0, t_E):
-    u = np.sqrt(np.square((time-t_0)/t_E) + u_0*u_0)
-    u2 = np.square(u)
-    magnification = (u2 + 2.) / (u * np.sqrt(u2 + 4.))
-    fluxes = np.polyfit(magnification, obs_flux, 1, w=np.reciprocal(obs_flux_err))
+    u2 = ((time-t_0)/t_E)**2 + u_0**2
+    magnification = (u2 + 2.) / sqrt(u2 * (u2 + 4.))
+    fluxes = polyfit(magnification, obs_flux, 1, w=reciprocal(obs_flux_err))
     flux = fluxes[0] * magnification + fluxes[1]
-    chi2 = np.sum(np.square((obs_flux - flux) / obs_flux_err))
+    chi2 = sum(square((obs_flux - flux) / obs_flux_err))
     return chi2
