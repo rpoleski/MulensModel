a3100c50241cd57ae060c876d36f3f9b49aea424
Radek Poleski <radek.poleski@gmail.com>
2018-06-19 14:07:13 -0400

    speeding up parallax calculations

--- examples/run_time_tests/MM_setup_2_1000.py
+++ examples/run_time_tests/MM_setup_2_1000.py
@@ -12,7 +12,7 @@ u_0 = 0.1
 t_E = 50.
 pi_E_N = 0.6
 pi_E_E = 0.8
-param = {'t_0': t_0, 'u_0': u_0, 't_E': t_E, 'pi_E_N': pi_E_N, 'pi_E_E': pi_E_E}
+param = {'t_0': t_0, 'u_0': u_0, 't_E': t_E, 'pi_E_N': pi_E_N, 'pi_E_E': pi_E_E, 't_0_par': t_0}
 model = mm.Model(param, coords="18:00:00.00 -30:00:00.0")
 event = mm.Event(datasets=[data_1], model=model)
 
