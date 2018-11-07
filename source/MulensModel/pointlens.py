a86a566cbaaa074c0df85398568aede333d6ead2
Radek Poleski <radek.poleski@gmail.com>
2018-11-07 16:58:13 -0500

    faster Lee+09 calculations

--- source/MulensModel/pointlens.py
+++ source/MulensModel/pointlens.py
@@ -369,8 +369,8 @@ class PointLens(object):
         Stable Formalism"
         <http://adsabs.harvard.edu/abs/2009ApJ...695..200L>`_
         """
-        n_theta = 80
-        n_u = 100
+        n_theta = 90
+        n_u = 1000
 
         mag = np.zeros_like(u)
 
@@ -387,22 +387,20 @@ class PointLens(object):
         """
         values = 1. - (u_**2 - 2. * u_ * u * cos(theta_) + u**2) / rho**2
         values[-1] = 0.
-# just for de-bugging:
-#        mask = values<0
-#        if np.any(mask):
-#            print(u_[mask])
-#            print(values[mask])
-#            print(u_)
         out = 1. - gamma * (1. - 1.5 * np.sqrt(values))
         return out * (u_**2 + 2.) / np.sqrt(u_**2 + 4.)
 
-# XXX unused code - REMOVE
-        out = np.zeros_like(u_)
-        mask = (values >= 0.) # XXX
-        out[mask] = np.sqrt(values[mask])
-        out = 1. - gamma * (1. - 1.5 * out)
-        out *= (u_**2 + 2.) / np.sqrt(u_**2 + 4.)
-        return out
+    def _integrand_Lee09_v2(self, u_, u, theta_, rho, gamma):
+        """
+        Integrand in Equation 13 in Lee et al. 2009.
+
+        u_ and theta_ are np.ndarray, other parameters are scalars.
+        theta_ is in fact cos(theta_) here
+        """
+        values = 1. - (u_**2 - 2. * u_ * u * theta_ + u**2) / rho**2
+        values[:,-1] = 0.
+        out = 1. - gamma * (1. - 1.5 * np.sqrt(values))
+        return out * (u_**2 + 2.) / np.sqrt(u_**2 + 4.)
 
     def _LD_Lee09(self, u, rho, gamma, n_theta, n_u):
         """
@@ -418,33 +416,44 @@ class PointLens(object):
         else:
             theta_max = None
             theta_max = np.pi
-#        theta = np.linspace(0, theta_max-1e-10, n_theta) # XXX
-        theta = np.linspace(0, theta_max-1e-4, n_theta) # XXX
+        theta = np.linspace(0, theta_max-1e-5, n_theta) # XXX
         integrand_values = np.zeros_like(theta)
         u_1 = self._u_1_Lee09(theta, u, rho, theta_max)
+        u_1 += 1.e-13 # XXX
         u_2 = self._u_2_Lee09(theta, u, rho, theta_max)
 
+        #if False:
+        if True:
+# First version
+            temp = np.zeros( (len(theta), n_u) )
+            temp2 = (np.zeros( (len(theta), n_u) ).T + np.cos(theta)).T
+            for (i, (theta_, u_1_, u_2_)) in enumerate(zip(theta, u_1, u_2)):
+                if u_1_ == 0. and u_2_ == 0.:
+                    continue
+                temp[i] = np.linspace(u_1_, u_2_, n_u)
+            integrand = self._integrand_Lee09_v2(temp, u, temp2, rho, gamma)
+            for (i, (theta_, u_1_, u_2_)) in enumerate(zip(theta, u_1, u_2)):
+                if u_1_ == 0. and u_2_ == 0.:
+                    continue
+                integrand_values[i] = integrate.simps(integrand[i], dx=temp[i, 1]-temp[i, 0])
+            #out = integrate.simps(integrand_values, theta)
+            out = integrate.simps(integrand_values, dx=theta[1]-theta[0])
+            out *= 2. / (np.pi * rho**2)
+            return out
+
+# Second version
         for (i, (theta_, u_1_, u_2_)) in enumerate(zip(theta, u_1, u_2)):
-            # XXX do we need 2 lines below ?
-            if u_1_ == 0. and u_2_ == 0.:
-                continue
+            # XXX do we need 2 lines below ? It seems to slow down.
+            #if u_1_ == 0. and u_2_ == 0.:
+                #continue
+
 # XXX - correct this:
-#            du_ = (u_2_ - u_1_) / (n_u-1)
-#            u_ = np.arange(u_1_, u_2_+du_, du_)
 # https://github.com/numpy/numpy/blob/v1.15.0/numpy/core/function_base.py#L25-L150
-# div = (num - 1)
-# y = _nx.arange(0, num, dtype=dt)
-# delta = stop - start
-# step = delta / div
-# y *= step
-# y += start
             u_ = np.linspace(u_1_, u_2_, n_u)
             integrand = self._integrand_Lee09(u_, u, theta_, rho, gamma)
             integrand_values[i] = integrate.simps(integrand, dx=u_[1]-u_[0])
-#            integrand_values[i] = integrate.simps(integrand, u_)
 
-        out = integrate.simps(integrand_values, theta)
+        out = integrate.simps(integrand_values, dx=theta[1]-theta[0])
         out *= 2. / (np.pi * rho**2)
 
         return out
-
