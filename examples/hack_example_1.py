fdfb16803101d56618c2459f70588a52e79beac2
Radek Poleski <radek.poleski@gmail.com>
2019-01-27 20:45:59 -0500

    hack session examples

---
+++ examples/hack_example_1.py
@@ -0,0 +1,54 @@
+import os
+import numpy as np
+import scipy.optimize as op
+from matplotlib import pyplot as plt
+from matplotlib import gridspec
+
+import MulensModel as MM
+
+
+def chi2_fun(theta, event, parameters_to_fit):
+    """
+    Calculate chi^2 for *event* with *parameters_to_fit*
+    set to corresponding *theta* values.
+    """
+    for (theta_, param) in zip(theta, parameters_to_fit):
+        setattr(event.model.parameters, param, theta_)
+    return event.get_chi2()
+
+
+# Read in the data file.
+SAMPLE_FILE_01 = os.path.join(
+    MM.MODULE_PATH, "data", "photometry_files",
+    "phot_ob08092_O4.dat")
+data = MM.MulensData(file_name=SAMPLE_FILE_01)
+
+# Initialize the fit.
+parameters = ["t_0", "u_0", "t_E"]
+initial_guess = [5380., 0.5, 20.]
+model = MM.Model(dict(zip(parameters, initial_guess)))
+event = MM.Event(datasets=data, model=model)
+
+# Find the best-fit parameters.
+result = op.minimize(
+    chi2_fun, x0=initial_guess, args=(event, parameters),
+    method='Nelder-Mead')
+
+# Print results.
+print("{:.5f} {:.4f} {:.3f}".format(*result.x))
+(fit_t_0, fit_u_0, fit_t_E) = result.x
+chi2 = chi2_fun(result.x, event, parameters)
+print("{:.3f}".format(chi2))
+
+# Plot results.
+best = [event.best_chi2_parameters[p] for p in parameters]
+chi2_fun(best, event, parameters) # This allows plotting of the best model.
+grid = gridspec.GridSpec(2, 1, height_ratios=[5, 1])
+plt.subplot(grid[0])
+event.plot_data()
+event.plot_model(t_start=5340., t_stop=5420.)
+plt.xlim(5340., 5420.)
+plt.subplot(grid[1])
+event.plot_residuals()
+plt.xlim(5340., 5420.)
+plt.show()
---
+++ examples/hack_example_2.py
@@ -0,0 +1,147 @@
+import os
+import sys
+import numpy as np
+import emcee
+from matplotlib import pyplot as plt
+import configparser
+
+import MulensModel as MM
+
+import example_read as read
+
+
+def ln_like(theta, event, parameters_to_fit, print_models):
+    """
+    Likelihood function. The values of *parameters_to_fit* are in *theta*.
+    MulensModel Event class instance *event* gives event for which
+    calculations will be done. Boolean *print_models* controls if
+    all models are printed.
+    """
+    for (theta_, param) in zip(theta, parameters_to_fit):
+        setattr(event.model.parameters, param, theta_)
+    chi2 = event.get_chi2()
+    if print_models:
+        print(chi2, *[t for t in theta], flush=True)
+    return -0.5 * chi2
+
+def ln_prior(theta, parameters_to_fit):
+    """
+    Prior. Check if *theta* values for *parameters_to_fit* are within ranges
+    defined by *ln_prior.min* and *ln_prior.max*.
+    """
+    inside = 0.
+    outside = -np.inf
+
+    for (parameter, value) in ln_prior.min.items():
+        index = parameters_to_fit.index(parameter)
+        if theta[index] < value:
+            return outside
+
+    for (parameter, value) in ln_prior.max.items():
+        index = parameters_to_fit.index(parameter)
+        if theta[index] > value:
+            return outside
+
+    return inside
+
+def ln_prob(
+        theta, event, parameters_to_fit, print_models=False):
+    """
+    Log probability of the model - combines ln_prior() and ln_like().
+    """
+    ln_prior_ = ln_prior(theta, parameters_to_fit)
+    if not np.isfinite(ln_prior_):
+        return -np.inf
+
+    ln_like_ = ln_like(theta, event, parameters_to_fit, print_models)
+    if np.isnan(ln_like_):
+        return -np.inf
+
+    return ln_prior_ + ln_like_
+
+def generate_random_parameters(parameters, starting, n):
+    """
+    Generate *n* vectors of values of *parameters* according to distributions
+    specified in *starting*.
+    """
+    values = []
+    for param in parameters:
+        settings = starting[param]
+        if settings[0] == 'gauss':
+            v = settings[2] * np.random.randn(n)
+            v += settings[1]
+        elif settings[0] == 'uniform':
+            v = np.random.uniform(
+                low=settings[1], high=settings[2], size=n)
+        elif settings[0] == 'log-uniform':
+            beg = np.log(settings[1])
+            end = np.log(settings[2])
+            v = np.exp(np.random.uniform(beg, end, n))
+        values.append(v)
+    return np.array(values).T.tolist()
+
+
+# Read config file.
+if len(sys.argv) != 2:
+    raise ValueError('Exactly one argument needed - cfg file')
+config_file = sys.argv[1]
+
+config = configparser.ConfigParser()
+config.optionxform = str
+config.read(config_file)
+files = read.read_files_from_config(config)
+model_settings = read.read_model_settings(config)
+(parameters, starting) = read.read_parameters_start(config)
+(min_values, max_values) = read.read_min_max(config)
+ln_prior.min = min_values
+ln_prior.max = max_values
+emcee_settings = read.read_emcee_settings(config)
+other_settings = read.read_other(config)
+
+# Read photometric data.
+datasets = [MM.MulensData(file_name=f[0], phot_fmt=f[1]) for f in files]
+
+# Generate starting values of parameters.
+start = generate_random_parameters(parameters, starting, emcee_settings['n_walkers'])
+
+# Setup Event instance that combines model and data.
+my_model = MM.Model(dict(zip(parameters, start[0])), coords=model_settings['coords'])
+if 'methods' in model_settings:
+    my_model.set_magnification_methods(model_settings['methods'])
+if 'default_method' in model_settings:
+    my_model.set_default_magnification_method(model_settings['default_method'])
+my_event = MM.Event(datasets=datasets, model=my_model)
+
+# Prepare sampler.
+n_dim = len(parameters)
+print_models = other_settings.get('print_models', False)
+args = (my_event, parameters, print_models)
+sampler = emcee.EnsembleSampler(emcee_settings['n_walkers'], n_dim, ln_prob, args=args)
+
+# Run sampler.
+sampler.run_mcmc(start, emcee_settings['n_steps'])
+
+# Parse results.
+burn = emcee_settings['n_burn']
+samples = sampler.chain[:, burn:, :].reshape((-1, n_dim))
+r_16 = np.percentile(samples, 16, axis=0)
+r_50 = np.percentile(samples, 50, axis=0)
+r_84 = np.percentile(samples, 84, axis=0)
+print("Fitted parameters:")
+for i in range(n_dim):
+    if parameters[i] == 'q':
+        fmt = "{:} {:.7f} +{:.7f} -{:.7f}"
+    else:
+        fmt = "{:} {:.5f} +{:.5f} -{:.5f}"
+    print(fmt.format(parameters[i], r_50[i], r_84[i]-r_50[i], r_50[i]-r_16[i]))
+print("Smallest chi2 model:")
+best = [my_event.best_chi2_parameters[p] for p in parameters]
+print(*[b if isinstance(b, float) else b.value for b in best])
+print(my_event.best_chi2)
+
+# Plot results.
+ln_like(best, my_event, parameters, False) # This allows plotting of the best model.
+my_event.plot_data(subtract_2450000=True)
+my_event.plot_model(subtract_2450000=True)
+plt.xlim(*other_settings['plot_time'])
+plt.show()
