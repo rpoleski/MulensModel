e2853279ec94041d0fbb3360c7cd8c661ecd4728
Radek Poleski <radek.poleski@gmail.com>
2019-04-25 15:33:56 -0400

    PEP8 in setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,16 @@ import warnings
 from setuptools import setup, Extension
 
 
-dir_ = os.path.join('lib', 'python' + sys.version[:3], 'site-packages', 
-                    'MulensModel')
-
 source_VBBL = os.path.join('source', 'VBBL')
 source_AC = os.path.join('source', 'AdaptiveContouring')
 source_MM = os.path.join('source', 'MulensModel')
 source_MMmo = os.path.join(source_MM, 'mulensobjects')
 
-data_files = [ (os.path.join(dir_, 'data'), [os.path.join('data', 'interpolation_table_b0b1_v1.dat')]) ]
+dir_ = os.path.join('lib', 'python' + sys.version[:3], 'site-packages',
+                    'MulensModel')
+dir_1 = os.path.join(dir_, 'data')
+file_1 = os.path.join('data', 'interpolation_table_b0b1_v1.dat')
+data_files = [(dir_1, [file_1])]
 
 version = "unknown"
 with open(os.path.join('source', 'MulensModel', 'version.py')) as in_put:
@@ -22,7 +23,8 @@ with open(os.path.join('source', 'MulensModel', 'version.py')) as in_put:
         if line_.startswith('__version__'):
             version = line_.split()[2][1:-1]
 
-ext_AC = Extension('MulensModel.AdaptiveContouring', glob.glob(source_AC+"/*.c"))
+ext_AC = Extension('MulensModel.AdaptiveContouring',
+                   glob.glob(source_AC+"/*.c"))
 ext_VBBL = Extension('MulensModel.VBBL', glob.glob(source_VBBL+"/*.cpp"))
 
 setup(
@@ -40,4 +42,3 @@ setup(
     data_files=data_files,
     install_requires=['numpy', 'matplotlib', 'scipy', 'astropy>=1.2.0']
 )
-
