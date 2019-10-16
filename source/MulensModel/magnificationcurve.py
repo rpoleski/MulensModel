4ac4e503cb619b4c663b089e951448f99dbce6ae
jenniferyee <jennifer.c.yee@gmail.com>
2019-10-16 16:07:47 -0400

    moved magnification property to end.

--- source/MulensModel/magnificationcurve.py
+++ source/MulensModel/magnificationcurve.py
@@ -150,15 +150,6 @@ class MagnificationCurve(object):
         """
         self._methods_parameters = methods_parameters
 
-    @property
-    def magnification(self):
-        """
-        *np.ndarray*
-
-        provide vector of magnifications
-
-        """
-        return self.get_magnification()
 
     def get_magnification(self):
         """
@@ -426,3 +417,13 @@ class MagnificationCurve(object):
                if (value > 0 and value < n_max) else self._default_method
                for value in brackets]
         return out
+
+    @property
+    def magnification(self):
+        """
+        *np.ndarray*
+
+        provide vector of magnifications
+
+        """
+        return self.get_magnification()
