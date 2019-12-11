8868fe37eb656291a31012319ada18b06ea1d4c9
Radek Poleski <radek.poleski@gmail.com>
2019-12-11 12:00:42 -0500

    UC35 updated

--- examples/use_cases/use_case_35_caustics_3L.py
+++ examples/use_cases/use_case_35_caustics_3L.py
@@ -16,31 +16,34 @@ model = mm.Model(parameters)
 model_parameters = mm.ModelParameters(parameters)
 
 # Basic plotting:
-model.plot_trajectory(caustics=True)
+model.plot_trajectory(caustics=True, lenses=True)  # 'lenses" is a new keyword.
+# It plots dots at positions of lenses.
 plt.show()
 
 # Plot using mm.Caustics:
 keys = {'s_21', 's_31', 'psi', 'q_21', 'q_31'}  # We need only these keys
 caustic_parameters = {key: parameters[key] for key in keys}
 # New initialization of Caustics:
-caustics_1 = mm.Caustics(parameters_3L=caustic_parameters)
+caustics_1 = mm.Caustics(**caustic_parameters)
 caustics_1.plot()
 plt.show()
 
 # Find positions of the components and use them to plot the caustic:
-positions = model_parameters.lens_components_positions  # This is new property.
-print(positions)  # Prints a list of 3 complex numbers
+positions = model_parameters.lens_positions()  # This is new function. One can
+# add parameter: epoch=2456789.01 so that orbital motion is taken into account.
+print(positions)  # Prints a (3,2) numpy array
 caustics_2 = mm.Caustics(
     q_21=parameters['q_21'], q_31=parameters['q_31'],
-    lens_components_positions=positions)
+    lens_positions=positions)  # This keyword should also work for binary lens.
 caustics_2.plot()
 plt.show()
 
 # Change position of one of the components and then plot the caustic:
 # (this is why we want two ways to init Caustics for 3L events)
-positions[1] += 0.1 + 0.2j
+positions[1, 0] += 0.1
+positions[1, 1] += 0.5
 caustics_3 = mm.Caustics(
     q_21=parameters['q_21'], q_31=parameters['q_31'],
-    lens_components_positions=positions)
+    lens_positions=positions)
 caustics_3.plot()
 plt.show()
