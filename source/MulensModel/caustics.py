9a79b7d2c070d61e9cf715a86b7e65f567b59197
Radek Poleski <radek.poleski@gmail.com>
2019-12-18 16:57:36 -0500

    most of triple lens caustic calculation added

--- source/MulensModel/caustics.py
+++ source/MulensModel/caustics.py
@@ -18,12 +18,35 @@ class Caustics(object):
         s: *float*
             separation between the 2 bodies (as a fraction of the
             Einstein ring)
+
+        XXX - OTHER PARAMETERS
     """
 
-    def __init__(self, q, s):
-        self._n_lenses = 2
-        self.q = q  # XXX NEEDS DOCSTRING?
-        self.s = s
+    def __init__(self,
+                 q=None, s=None,
+                 q_21=None, q_31=None, s_21=None, s_31=None, psi=None):
+        key = {
+            'q': q is not None, 's': s is not None, 'q_21': q_21 is not None,
+            'q_31': q_31 is not None, 's_21': s_21 is not None,
+            's_31': s_31 is not None, 'psi': psi is not None}
+        keys_3L = ['q_21', 'q_31', 's_21', 's_31', 'psi']
+        if key['q'] and key['s']:
+            if np.any([key[k] for k in keys_3L]):
+                raise ValueError('XXX')
+            self._n_lenses = 2
+            self.q = q  # XXX NEEDS DOCSTRING?
+            self.s = s
+        elif np.all([key[k] for k in keys_3L]):
+            if key['q'] or key['s']:
+                raise ValueError('XXX')
+            self._n_lenses = 3
+            self._q_21 = q_21
+            self._q_31 = q_31
+            self._s_21 = s_21
+            self._s_31 = s_31
+            self._psi = psi
+        else:
+            raise ValueError('XXX')
 
         # Set place holder variables
         self._x = None
@@ -94,6 +117,8 @@ class Caustics(object):
         """
         if self._n_lenses == 2:
             self._calculate_binary_lens(n_points)
+        elif self._n_lenses == 3:
+            self._calculate_triple_lens(n_points)
         else:
             raise ValueError(
                 'Wrong number of lenses: {:}'.format(self._n_lenses))
@@ -101,7 +126,7 @@ class Caustics(object):
     def _calculate_binary_lens(self, n_points):
         """
         Solve the caustics polynomial to calculate the critical curve
-        and caustic structure.
+        and caustic structure for triple lens.
 
         Based on Eq. 6 Cassan 2008 modified so origin is center of
         mass and larger mass is on the left. Uses complex coordinates.
@@ -149,6 +174,53 @@ class Caustics(object):
         complex_conjugate = np.conjugate(complex_value)
         return complex_value - (1. / (1. + self.q)) * (
             (1./complex_conjugate) + (self.q / (complex_conjugate - self.s)))
+
+    def _calculate_triple_lens(self, n_points):
+        """
+        Solve the caustics polynomial to calculate the critical curve
+        and caustic structure for triple lens.
+        """
+        xy = Utils._parameters_to_center_of_mass_coords_3L(
+            self._q_21, self._q_31, self._s_21, self._s_31, self._psi)
+        z_1 = np.complex(xy[0, 0], x[0, 1])
+        z_2 = np.complex(xy[1, 0], x[1, 1])
+        z_3 = np.complex(xy[2, 0], x[2, 1])
+
+        epsilon_1 = 1. / (1. + self._q_21 + self._q_31)
+        epsilon_2 = epsilon_1 * self._q_21
+        epsilon_3 = epsilon_1 * self._q_31
+
+        self._calculate_triple_lens_complex(
+            z_1, z_2, z_3, epsilon_1, epsilon_2, epsilon_3, n_points)
+
+    def _calculate_triple_lens_complex(
+            self, z_1, z_2, z_3, epsilon_1, epsilon_2, epsilon_3, n_points):
+        """
+        XXX
+        """
+        n_angles = int(n_points/6.+.5)
+
+        # Initialize variables
+        self._x = []
+        self._y = []
+        # XXX self._critical_curve = self.CriticalCurve()
+
+        z_1_bar = np.conjugate(z_1)
+        z_2_bar = np.conjugate(z_2)
+        z_3_bar = np.conjugate(z_3)
+
+        for phi in np.linspace(0., 2.*np.pi, n_angles, endpoint=False):
+            polynomial = self._polynomial_3L(
+                z_1, z_2, z_3, epsilon_1, epsilon_2, epsilon_3, self._phi)
+            roots = np.polynomial.polynomial.polyroots(polynomial)
+            for root in roots:
+                # XXX critical curve: (root.real, root.imag)
+                root_bar = np.conjugate(root)
+                z = (root + epsilon_1 / (z_1_bar - root_bar) +
+                     epsilon_2 / (z_2_bar - root_bar) +
+                     epsilon_3 / (z_3_bar - root_bar))
+                self._x.append(z.real)
+                self._y.append(z.imag)
 
     class CriticalCurve(object):
         """
