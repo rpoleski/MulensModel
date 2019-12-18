e09d83477d645959577fb2cddd5ed2783e8a165c
Radek Poleski <radek.poleski@gmail.com>
2019-12-18 15:58:40 -0500

    UC36 - final version

--- examples/use_cases/use_case_36_degeneracies.py
+++ examples/use_cases/use_case_36_degeneracies.py
@@ -17,43 +17,24 @@ coords = "18:04:45.71 -26:59:15.2"
 
 model_1 = mm.Model(parameters, coords=coords)
 
-# There are 3 approaches:
-# First approach - a separate function for each degeneracy:
+# Proposed solution - a separate function for each degeneracy:
 model_2 = model_1.predict_degenerate_jerk_model()
-model_3 = model_2.predict_degenerate_u_0_model()
+# There are two other degeneracies that can be implemented this way 
+# (i.e., discrete and requiring non-trivial calculations):
+# 1) satellite - Gould 1994 (1994ApJ...421L..75G) and
+#    Refsdal 1966 (1966MNRAS.134..315R),
+# 2) rho - Chung et al. 2017 (2017ApJ...838..154C).
+# References for jerk degeneracy:
+# Gould 2004 (2004ApJ...606..319G) and Park et al. 2004 (2004ApJ...609..166P).
 
-# Second approach - have a single function that deals with all degeneracies:
-model_2 = model_1.predict_degenerate_model(jerk=True)
-# and this allows combining multiple degeneracies:
-model_3 = model_1.predict_degenerate_model(jerk=True, u_0_sign=True)
-# other parameters we could have here are e.g.,
-# "close_wide" (would work only for binary lens events),
-# "ecliptic"...
-
-# Third approach - functions predicting degeneracies return a dict with
-# parameters to be changed and if the user wants to use them in
-# a Model instance, then they should copy the Model instance (which is not
-# implemented at this point) and change specific parameters.
-parameters_2 = model_1.predict_degenerate_jerk_model()
-parameters_3 = {'u_0': -model_1.parameters.u_0}
-model_2 = model_1.copy()
-# This copies not only parameters but coordinates etc.
-model_3 = model_1.copy()
-for (key, value) in parameters_2:
-    setattr(model_2.parameters, key, value)
-for (key, value) in parameters_3:
-    setattr(model_3.parameters, key, value)
-
-print("the 3 models are:")
+print("the 2 models are:")
 print(model_1)
 print(model_2)
-print(model_3)
 
 # Get the chi2 values for these models:
 file_name = os.join(mm.DATA_PATH, "photometry_files", "OB140939",
                     "ob140939_OGLE.dat")
 data = mm.MulensData(file_name=file_name)
-models = [model_1, model_2, model_3]
-for model in models:
+for model in [model_1, model_2]:
     event = mm.Event(datasets=data, model=model)
     print(event.get_chi2())
