1883c9d25fe4ce6da59387083d3a097d3b9b73a4
jenniferyee <jennifer.c.yee@gmail.com>
2020-01-22 10:40:10 -0500

    Updated documentation. Added source_fluxes and blend_flux.

--- source/MulensModel/fitdata.py
+++ source/MulensModel/fitdata.py
@@ -3,15 +3,37 @@ import numpy as np
 
 class FitData:
     """
-    Fits source and blending fluxes for given data and model magnification.
+    Performs a least squares linear fit for given dataset and model to
+    determine the source flux(es) and (optionally) blend flux.
 
+    .. _keywords:
     Keywords :
-        data: :py:class:`MulensData` or *list* of :py:class:`MulensData`
-            Photometric data to be fitted.
+        dataset: :py:class:`~MulensModel.mulensdata.MulensData` object
+            A single photometric dataset to be fitted.
+
+        model: :py:class:`~MulensModel.model.Model` object
+            The model to fit to the data.
+
+        fix_blend_flux: *False* or *float*
+            Default is *False*, i.e. allow the blend flux to be a free
+            parameter. If set to a float, it will fix the blend value to that
+            value.
+
+        fix_source_flux: *False* or *float*
+            Default is *False*, i.e. allow the source flux to be a free
+            parameter. If set to a float, it will fix the source value to that
+            value. (Not Implemented)
+
+        fix_q_flux: *False* or *float*
+            For binary source models, q_flux is the flux ratio between two
+            components, i.e. q_flux = f_source_2 / f_source_1
+            Default is *False*, i.e. allow the source flux to be a free
+            parameter. If set to a float, it will fix the source value to that
+            value. (Not Implemented)
 
     """
 
-    def __init__(self, model, dataset, fix_blend_flux=False,
+    def __init__(self, model=None, dataset=None, fix_blend_flux=False,
                  fix_source_flux=False, fix_q_flux=False):
         self._model = model
         self._dataset = dataset
@@ -22,20 +44,22 @@ class FitData:
         self.fix_q_flux = fix_q_flux
 
         # list containing fluxes of various sources
-        self.source_fluxes = None
-        self.blend_flux = None
+        self._source_fluxes = None
+        self._blend_flux = None
 
     def fit_fluxes(self):
         """
-        Function to perform
-        Arguments:
-        Returns itself
-
-        Btw this is too long for a function..
+        Execute the linear least squares fit to determine the fitted fluxes.
+        Sets the values of :py:obj:`~source_fluxes`, :py:obj:`~blend_flux`,
+        and (if applicable):py:obj:`~source_flux`.
         """
 
-        if self.fix_source_flux is True:
-            msg = 'Source flux can only be false'
+        if self.fix_source_flux is not False:
+            msg = 'Only fix_source_flux=False is implemented.'
+            raise NotImplementedError(msg)
+
+        if self.fix_q_flux is not False:
+            msg = 'Only fix_q_flux=False is implemented.'
             raise NotImplementedError(msg)
 
         n_sources = self._model.n_sources
@@ -72,6 +96,7 @@ class FitData:
         # Only deal with good data
         good_mag_matrix = mag_matrix[select]
 
+        # Can't this be rewritten to use vstack or hstack?
         if self.fix_blend_flux is False:
             x[0:n_sources, ] = good_mag_matrix
             # Row corresponding to blend flux
@@ -110,20 +135,54 @@ class FitData:
         # Record the results
 
         if self.fix_blend_flux is False:
-            self.blend_flux = results[-1]
-            self.source_fluxes = results[:-1]
+            self._blend_flux = results[-1]
+            self._source_fluxes = results[:-1]
         else:
-            self.blend_flux = self.fix_blend_flux
-            self.source_fluxes = results
+            self._blend_flux = self.fix_blend_flux
+            self._source_fluxes = results
 
     @property
     def source_flux(self):
+        """
+
+        :return: *float*
+            the fitted source flux. Only defined for models with a single
+            source. See also :py:obj:`~source_fluxes`
+
+            If None, you need to run :py:func:`~fit_fluxes()` to execute the
+            linear fit.
+
+        """
         if self._model.n_sources == 1:
             return self.source_fluxes[0]
         else:
             msg = ("source_flux is defined only for models" +
                    " with one source, you have" +
                    " {0}".format(self._model.n_sources) +
-                   " sources. Try FitData.source_fluxes")
+                   " sources. Try FitData.source_fluxes instead")
 
             raise NameError(msg)
+
+    @property
+    def source_fluxes(self):
+        """
+
+        :return: *np.array*
+            the fitted source flux(es).
+
+            If None, you need to run :py:func:`~fit_fluxes()` to execute the
+            linear fit.
+        """
+        return self._source_fluxes
+
+    @property
+    def blend_flux(self):
+        """
+
+        :return: the fitted blend flux or the value set by
+            fix_blend_flux (see :ref:`keywords`).
+
+            If None, you need to run :py:func:`~fit_fluxes()` to execute the
+            linear fit.
+        """
+        return self._blend_flux
