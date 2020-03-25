d8fc6e9a9d34a2aa835d5f7db6a6d8ec6f8fdcad
jenniferyee <jennifer.c.yee@gmail.com>
2020-03-25 10:29:46 -0400

    simplified passing of t_0_par

--- source/MulensModel/tests/test_Model_Parallax.py
+++ source/MulensModel/tests/test_Model_Parallax.py
@@ -213,13 +213,12 @@ def test_annual_parallax_calculation_6():
 def test_satellite_and_annual_parallax_calculation():
     """test parallax calculation with Spitzer data"""
     model_parameters = {'t_0': 2456836.22, 'u_0': 0.922, 't_E': 22.87,
-                               'pi_E_N': -0.248, 'pi_E_E': 0.234}
+                        'pi_E_N': -0.248, 'pi_E_E': 0.234, 't_0_par': 2456836.2}
     coords = "17:47:12.25 -21:22:58.2"
-    
+
     model_with_par = mm.Model(model_parameters, coords=coords)
     model_with_par.parallax(satellite=True, earth_orbital=True,
                             topocentric=False)
-    model_with_par.parameters.t_0_par = 2456836.2
 
     data_OGLE = mm.MulensData(file_name=SAMPLE_FILE_02)
     data_Spitzer = mm.MulensData(
