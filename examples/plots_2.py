395611d4968decb6292acdaafc13ead6bb8c7d87
Radek Poleski <rpoleski@astrouw.edu.pl>
2020-07-08 15:27:27 +0200

    reference corrected in plots_*.py

--- examples/plots_2.py
+++ examples/plots_2.py
@@ -1,5 +1,5 @@
 """
-Create Figure 2. of Poleski and Yee (2009)
+Create Figure 2. of Poleski and Yee (2019)
 "Modeling microlensing events with MulensModel"
 Astronomy and Computing 26, 35
 https://ui.adsabs.harvard.edu/abs/2019A&C....26...35P/abstract
