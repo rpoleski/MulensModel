97c782213ada1f3c6f34d9e30390152507e36bf1
jenniferyee <jennifer.c.yee@gmail.com>
2021-05-21 10:21:58 -0400

    Updated to reflect new_fit implementation. Works.

--- examples/use_cases/use_case_26b_binary_source_model.py
+++ examples/use_cases/use_case_26b_binary_source_model.py
@@ -3,9 +3,6 @@ import numpy as np
 
 import MulensModel as mm
 
-
-raise NotImplementedError('This use case has not been implemented.')
-
 # Model parameters
 t_0_1 = 2458000.
 u_0_1 = 0.1
@@ -38,17 +35,18 @@ combined_model_lc = mm.Utils.get_mag_from_flux(
 
 # Binary Source Model version
 binary_source_model = mm.Model(dict(
-    t_0_1=t_0_1, u_0_1=u_0_1, t_0_2=t_0_2, u_0_2=u_0_2, t_E=t_E,
-    flux_ratio=f_source_2/f_source_1))
+    t_0_1=t_0_1, u_0_1=u_0_1, t_0_2=t_0_2, u_0_2=u_0_2, t_E=t_E))
 
 # Plots
 plt.figure()
-plt.title('2 Point Source Models')
+plt.title('Sum of Two Point Source Models')
 plt.plot(times, combined_model_lc, color='blue')
 plt.gca().invert_yaxis()
 
 plt.figure()
-plt.title('1 Binary Source Model')
-binary_source_model.plot_lc(times, source_flux=f_source_1, color='magenta')
+plt.title('Binary Source Model')
+binary_source_model.plot_lc(
+    times, source_flux=f_source_1, source_flux_ratio=f_source_2/f_source_1,
+    blend_flux=f_blend, color='magenta')
 
 plt.show()
