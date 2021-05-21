cb84fb3add74a05d74982831730623676a3fc298
jenniferyee <jennifer.c.yee@gmail.com>
2021-05-21 10:14:16 -0400

    Updated to reflect new_fit implementation. Works.

--- examples/use_cases/use_case_25_flux_constraint.py
+++ examples/use_cases/use_case_25_flux_constraint.py
@@ -11,8 +11,6 @@ import os
 import MulensModel as mm
 
 
-raise NotImplementedError('This use case has not been implemented.')
-
 def chi2_fun(theta, event, parameters_to_fit):
     """
     for given event set attributes from parameters_to_fit (list of
@@ -40,10 +38,10 @@ def get_color_constraint(event):
     # Color constraint for OB161195 (I_KMT - L_Spitzer)
     (source_color, sigma) = (0.78, 0.03)
 
-    f_s_ogle = event.source_fluxes[datasets[KMT]]
-    f_s_spitzer = event.source_fluxes[dataset[Spitzer]]
+    (f_s_kmt, f_b_kmt) = event.get_flux_for_dataset(datasets[KMT])
+    (f_s_spitzer, f_b_spitzer) = event.get_flux_for_dataset(datasets[Spitzer])
 
-    color = -2.5 * np.log10(f_s_ogle / f_s_spitzer)
+    color = -2.5 * np.log10(f_s_kmt[0] / f_s_spitzer[0])
 
     return (color - source_color)**2 / sigma**2
 
@@ -54,22 +52,31 @@ file_names = ['KCT01I.dat', 'KCT41I.dat', 'KCT42I.dat', 'KSA01I.dat',
               'KSA41I.dat', 'KSA42I.dat', 'KSS01I.dat', 'KSS41I.dat',
               'KSS42I.dat', 'spitzer_b12.dat']
 dir_ = os.path.join(mm.DATA_PATH, "photometry_files", "OB161195")
+spitzer_ephemeris = os.path.join(
+            mm.DATA_PATH, "ephemeris_files", "Spitzer_ephemeris_02.dat")
 for file_name in file_names:
     file_ = os.path.join(dir_, file_name)
-    datasets.append(mm.MulensData(file_name=file_, add_2450000=True))
+    if file_name[0:7] == "spitzer":
+        ephemerides_file = spitzer_ephemeris
+    else:
+        ephemerides_file = None
+
+    datasets.append(mm.MulensData(
+        file_name=file_, add_2450000=True, ephemerides_file=ephemerides_file))
 
 # Close-- model
 model = mm.Model(
-    {'t_0': 2457568.7692, 'u_0': -0.05321, 't_E': 9.96, 'rho': 0.00290,
+    {'t_0': 2457568.7692, 'u_0': 0.05321, 't_E': 9.96, 'rho': 0.00290,
      'pi_E_N': -0.2154, 'pi_E_E': -0.380,
      'alpha': np.rad2deg(-0.9684), 's': 0.9842, 'q': 0.0000543})
 
-methods = [7560., 'VBBL', 7580.]
+methods = [2457560., 'VBBL', 2457580.]
 
 model.set_magnification_methods(methods)
 model.set_default_magnification_method('point_source_point_lens')
 
-event = mm.Event(datasets=datasets, model=model,
-                          coords="17:55:23.50 -30:12:26.1")
+coords = "17:55:23.50 -30:12:26.1"
+event = mm.Event(datasets=datasets, model=model, coords=coords)
 
 print(chi2_fun([], event, []))
+
