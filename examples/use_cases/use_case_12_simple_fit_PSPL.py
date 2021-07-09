b5c6bcce66bb6afe20b3c317991d966a77f25b89
jenniferyee <jennifer.c.yee@gmail.com>
2021-07-09 10:34:32 -0400

    Fixed so it actually works (add_2450000=True)

--- examples/use_cases/use_case_12_simple_fit_PSPL.py
+++ examples/use_cases/use_case_12_simple_fit_PSPL.py
@@ -8,21 +8,22 @@ import MulensModel as mm
 
 
 # Initial Model
-t_0 = 2457520.
-u_0 = 0.6
-t_E = 130.
+t_0 = 2455380.
+u_0 = 0.523
+t_E = 20.
 
 model = mm.Model({'t_0': t_0, 'u_0': u_0, 't_E': t_E})
 
 # Import data
 file_name = os.path.join(mm.DATA_PATH, 'photometry_files',
                          'OB08092', 'phot_ob08092_O4.dat')
-data = mm.MulensData(file_name=file_name)
+data = mm.MulensData(file_name=file_name, add_2450000=True)
 
 # Create Event
 event = mm.Event(datasets=data, model=model)
 print('Initial Model')
 print(event.model.parameters)
+print(event.get_chi2())
 
 
 def chi2(theta, event, parameters_to_fit):
@@ -32,16 +33,20 @@ def chi2(theta, event, parameters_to_fit):
     """
     for key, val in enumerate(parameters_to_fit):
         setattr(event.model.parameters, val, theta[key])
+
     return event.get_chi2()
 
 
 # Fit model to data using scipy
 parameters_to_fit = ["t_0", "u_0", "t_E"]
 initial_guess = [t_0, u_0, t_E]
-result = op.minimize(chi2, initial_guess, args=(event, parameters_to_fit))
+result = op.minimize(
+    chi2, initial_guess, args=(event, parameters_to_fit),
+    method='Nelder-Mead')
 (fit_t_0, fit_u_0, fit_t_E) = result.x
 
 # Save results and print.
 chi2(result.x, event, parameters_to_fit)
 print('Fitted Model')
 print(event.model.parameters)
+print(event.get_chi2())
