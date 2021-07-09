ff11c46c916677d6d1f10abc316ce26ddf248777
jenniferyee <jennifer.c.yee@gmail.com>
2021-07-09 10:00:41 -0400

    Updated so model parameters match the dataset.

--- examples/use_cases/use_case_10_get_chi2.py
+++ examples/use_cases/use_case_10_get_chi2.py
@@ -12,20 +12,24 @@ import MulensModel as mm
 # Load the data
 data = mm.MulensData(
     file_name=os.path.join(
-        mm.DATA_PATH, 'photometry_files', 'OB08092', 'phot_ob08092_O4.dat'))
+        mm.DATA_PATH, 'photometry_files', 'OB08092', 'phot_ob08092_O4.dat'),
+    add_2450000=True)
 
 # Define the model
 model = mm.Model(
-    {'t_0': 2457518.902, 'u_0': 0.590, 't_E': 133.34*u.day})
+    {'t_0': 2455379.571, 'u_0': 0.523, 't_E': 17.94 })
 
 # Combine the model and the data
 event = mm.Event(datasets=data, model=model)
 print(event.get_chi2())
 
 # Get magnifications for selected dates
-model_times = np.arange(2457200, 2457800, 100.)
+model_times = np.arange(2455300, 2455400, 10.)
 print(model_times)
 print(event.model.magnification(model_times))
+
+event.plot_data()
+event.plot_model()
 
 # Get fluxes for all datasets
 fmt = "dataset {:}: F_s = {:.3f} F_b = {:.3f}"
@@ -33,3 +37,5 @@ for (i, dataset) in enumerate(event.datasets):
     f_source = event.fits[i].source_flux
     f_blend = event.fits[i].blend_flux
     print(fmt.format(i, f_source, f_blend))
+
+pl.show()
