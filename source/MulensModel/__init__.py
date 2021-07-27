edbe3a2a5bc8546ad14292ae758bbc7850e0600e
Radek Poleski <rpoleski@astrouw.edu.pl>
2021-07-27 20:57:18 +0200

    correcting imports

--- source/MulensModel/__init__.py
+++ source/MulensModel/__init__.py
@@ -1,16 +1,26 @@
 from os import path
 
 from .version import __version__
-from MulensModel.event import *
-from MulensModel.fit import *
-from MulensModel.model import *
-from MulensModel.modelparameters import *
-from MulensModel.mulensdata import *
+
 from MulensModel.binarylens import BinaryLens
-from MulensModel.pointlens import PointLens
+from MulensModel.caustics import Caustics
+from MulensModel.coordinates import Coordinates
+from MulensModel.event import Event
+from MulensModel.fit import Fit
+from MulensModel.fitdata import FitData
+from MulensModel.horizons import Horizons
+from MulensModel.limbdarkeningcoeffs import LimbDarkeningCoeffs
+from MulensModel.magnificationcurve import MagnificationCurve
+from MulensModel.model import Model
+from MulensModel.modelparameters import ModelParameters, which_parameters
+from MulensModel.mulensdata import MulensData
+from MulensModel.pointlens import PointLens, get_pspl_magnification
+from MulensModel.satelliteskycoord import SatelliteSkyCoord
+from MulensModel.trajectory import Trajectory
+from MulensModel.uniformcausticsampling import UniformCausticSampling
+from MulensModel.utils import Utils, MAG_ZEROPOINT
 
 from MulensModel.mulensobjects import *
-from MulensModel.fitdata import FitData
 
 __all__ = ['mulensobjects', 'MODULE_PATH', 'DATA_PATH']
 
