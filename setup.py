104b40b269e8a27f22875c0c1c2ad0b1a77342ed
Radek Poleski <rpoleski@astrouw.edu.pl>
2021-12-06 19:40:48 +0100

    adding more files and some cleanup in setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,10 @@ from setuptools import setup, Extension, find_packages
 
 file_required = "requirements.txt"
 
-source_VBBL = os.path.join('source', 'VBBL')
-source_AC = os.path.join('source', 'AdaptiveContouring')
+source_VBBL_abs = os.path.abspath(os.path.join('source', 'VBBL'))
+source_AC_abs = os.path.abspath(os.path.join('source', 'AdaptiveContouring'))
 source_MM = os.path.join('source', 'MulensModel')
 source_MMmo = os.path.join(source_MM, 'mulensobjects')
-
-source_VBBL_abs = os.path.abspath(source_VBBL)
-source_AC_abs = os.path.abspath(source_AC)
 
 # Read all files from data/ in format adequate for data_files option of setup.
 files = glob.glob(os.path.join("data", "**", "*"), recursive=True)
@@ -25,22 +22,30 @@ for file_ in files:
         dir_files[dir_] += [file_]
     else:
         dir_files[dir_] = [file_]
+dir_files["."] = [file_required, "README.md", "LICENSE"]
+files_ = ["Horizons_manual.md", "examples_list.md", "papers_to_cite.md",
+          "magnification_methods.pdf", "parameter_names.pdf"]
+dir_files["documents"] = [os.path.join("documents", f) for f in files_]
+dir_files["examples"] = glob.glob(os.path.join("examples", "*.py"))
+dir_ex_16 = os.path.join("examples", "example_16")
+dir_ex_16_files = ["ulens_model_fit.py", "ulens_model_plot.py"]
+dir_files[dir_ex_16] = [os.path.join(dir_ex_16, f) for f in dir_ex_16_files]
 data_files = []
-dir_files["."] = [file_required]
 for (key, value) in dir_files.items():
     data_files += [(os.path.join('MulensModel', key), value)]
 
+# Read ther version.
 version = "unknown"
 with open(os.path.join('source', 'MulensModel', 'version.py')) as in_put:
     for line_ in in_put.readlines():
         if line_.startswith('__version__'):
             version = line_.split()[2][1:-1]
 
+# Prepare extensions and required modules.
 ext_AC = Extension('MulensModel.AdaptiveContouring', libraries=["m"],
                    sources=glob.glob(os.path.join(source_AC_abs, "*.c")))
 ext_VBBL = Extension('MulensModel.VBBL', libraries=["m"],
                      sources=glob.glob(os.path.join(source_VBBL_abs, "*.cpp")))
-
 with open(file_required) as file_:
     required = file_.read().splitlines()
 
@@ -49,10 +54,10 @@ setup(
     version=version,
     url='https://github.com/rpoleski/MulensModel',
     project_urls={
-        'git clone': 'git@github.com:rpoleski/MulensModel.git',
+#        'git clone': 'git@github.com:rpoleski/MulensModel.git',
         'documentation': 'https://github.com/rpoleski/MulensModel'},
     ext_modules=[ext_AC, ext_VBBL],
-    author='Radek Poleski',
+    author='Radek Poleski & Jennifer Yee',
     author_email='radek.poleski@gmail.com',
     description='package for modeling gravitational microlensing events',
     packages=['MulensModel', 'MulensModel.mulensobjects'],
