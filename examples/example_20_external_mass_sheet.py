ef678406d53ffb73cd0075323d2f8073c21f4dab
alpv95 <59vpla@gmail.com>
2022-01-18 09:27:40 -0800

    pep8

--- examples/example_20_external_mass_sheet.py
+++ examples/example_20_external_mass_sheet.py
@@ -13,29 +13,29 @@ import MulensModel as mm
 #Define lens model and source parameters
 s = 1.0
 q = 0.01
-alpha = 270 
+alpha = 270
 K = 0.1
-G = complex(0.05,0.0)
+G = complex(0.05, 0.0)
 t_0 = 300
 t_E = 500
 rho = 1e-5
 u_0 = -0.1
 
-Day = np.arange(-500,1000,1,dtype=float)
+Day = np.arange(-500, 1000, 1, dtype=float)
 
 lens = mm.model.Model(
-        {'t_0': t_0, 'u_0': u_0, 't_E': t_E, 's': s, 'q': q, 'alpha': alpha, 'convergence_K': K, 
-        'shear_G': G, 'rho':rho})
+    {'t_0': t_0, 'u_0': u_0, 't_E': t_E, 's': s, 'q': q, 'alpha': alpha, 'convergence_K': K,
+     'shear_G': G, 'rho': rho})
 
 lens.set_magnification_methods([min(Day), 'vbbl', max(Day)])
 
 #Plot magnification curve and caustics
-fig, (ax1,ax2) = plt.subplots(figsize=(10,5),ncols=2)
+fig, (ax1, ax2) = plt.subplots(figsize=(10, 5), ncols=2)
 
-ax1.plot(Day, lens.get_magnification(Day),color='r')
+ax1.plot(Day, lens.get_magnification(Day), color='r')
 
-ax2.set_xlim(-1,1)
-ax2.set_ylim(-1,1)
+ax2.set_xlim(-1, 1)
+ax2.set_ylim(-1, 1)
 ax2.set_xlabel("x", fontweight="bold")
 ax2.set_ylabel("y", fontweight="bold")
 lens.plot_trajectory(t_range=[t_0 - t_E, t_0], caustics=True, color='red',)
