093c66b62a177ec6b485035d3510832ccbaa2391
Keto Zhang <keto.zhang@gmail.com>
2022-02-01 02:07:18 -0800

    Revert importlib

--- source/MulensModel/__init__.py
+++ source/MulensModel/__init__.py
@@ -1,4 +1,3 @@
-from importlib import resources
 from os import path
 
 from MulensModel.binarylens import BinaryLens
@@ -27,4 +26,8 @@ MODULE_PATH = path.abspath(__file__)
 for i in range(3):
     MODULE_PATH = path.dirname(MODULE_PATH)
 
-DATA_PATH = str(resources.path("MulensModel", "data"))
+path_1 = path.join(MODULE_PATH, 'data')
+if path.isdir(path_1):
+    DATA_PATH = path_1
+else:
+    DATA_PATH = path.join(path.dirname(__file__), 'data')
