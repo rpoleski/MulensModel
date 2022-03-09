2ce5e27ff9e8f81943915171d8bdfea3785675e0
jenniferyee <jennifer.c.yee@gmail.com>
2022-03-09 09:53:32 -0500

    Updated delta_NE --> parallax_delta_N_E

--- source/MulensModel/fitdata.py
+++ source/MulensModel/fitdata.py
@@ -670,8 +670,8 @@ class FitData:
 
         # Below we deal with parallax only.
         if 'pi_E_N' in parameters or 'pi_E_E' in parameters:
-            delta_N = trajectory.delta_NE['N']
-            delta_E = trajectory.delta_NE['E']
+            delta_N = trajectory.parallax_delta_N_E['N']
+            delta_E = trajectory.parallax_delta_N_E['E']
 
             gradient['pi_E_N'] = d_u_d_x * delta_N + d_u_d_y * delta_E
             gradient['pi_E_E'] = d_u_d_x * delta_E - d_u_d_y * delta_N
