9a188f6c0f8f1622a852a619329d6fa97bdf9c1b
Radek Poleski <rpoleski@astrouw.edu.pl>
2022-03-19 13:33:52 +0100

    simplifying ex21

--- examples/example_21_external_mass_sheet.py
+++ examples/example_21_external_mass_sheet.py
@@ -15,7 +15,6 @@ import MulensModel as mm
 t_0 = 300
 u_0 = 0.25
 t_E = 500
-rho = 1.e-4
 s = 1.0
 q = 0.4
 alpha = 0
@@ -26,12 +25,9 @@ time = np.arange(t_0-775., t_0+775., 0.5, dtype=float)
 
 lens = mm.model.Model({
         't_0': t_0, 'u_0': u_0, 't_E': t_E, 's': s, 'q': q, 'alpha': alpha,
-        'convergence_K': K, 'shear_G': G, 'rho': rho})
+        'convergence_K': K, 'shear_G': G})
 no_shear = mm.model.Model({'t_0': t_0, 'u_0': u_0, 't_E': t_E, 's': s, 'q': q,
-                           'alpha': alpha, 'rho': rho})
-
-lens.set_magnification_methods([min(time), 'point_source', max(time)])
-no_shear.set_magnification_methods([min(time), 'point_source', max(time)])
+                           'alpha': alpha})
 
 # Plot magnification curve and caustics
 (fig, (ax1, ax2)) = plt.subplots(figsize=(10, 5), ncols=2)
@@ -44,8 +40,8 @@ ax2.set_ylim(-1.5, 1.5)
 ax2.set_xlabel("x", fontweight="bold")
 ax2.set_ylabel("y", fontweight="bold")
 no_shear.plot_trajectory(t_range=[t_0 - 475, t_0], caustics=True,
-		         color='green')
-mm.Caustics(s=s, q=q).plot(alpha=0.3)  # no_shear
+                         color='green')
+mm.Caustics(s=s, q=q).plot(alpha=0.3)
 lens.plot_trajectory(t_range=[t_0 - 475, t_0], caustics=True, color='green')
 lens.plot_trajectory(t_range=[t_0, t_0 + 475], color='blue')
 
