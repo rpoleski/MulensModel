f679583e13d47d6abf023c5e79384bc28acbdbbb
radek_poleski <radek.poleski@gmail.com>
2022-06-27 20:12:03 +0200

    cleanup in test_temp.py

--- source/MulensModel/tests/test_temp.py
+++ source/MulensModel/tests/test_temp.py
@@ -3,53 +3,57 @@ import unittest
 import MulensModel as mm
 
 
-t0 = 1000
-u0 = 0.1
-tE = 20
-K = -0.1
-G = complex(-0.1, -0.2)
-alpha = 123.
-da_dt = -0.3
+def test_single_lens_with_mass_sheet():
+    """
+    Test if Chang-Refsdal microlensing parameters are properly defined.
+    """
+    basic = {'t_0': 1000., 'u_0': 0.1, 't_E': 20.}
+    G = complex(-0.1, -0.2)
+    K = -0.1
 
-basic = {'t_0': t0, 'u_0': u0, 't_E': tE}
-
-def test_1():
     parameters = mm.ModelParameters({**basic})
-
-def test_2():
-    parameters = mm.ModelParameters({**basic, 'shear_G': G, 'alpha': alpha})
-
-def test_3():
+    parameters = mm.ModelParameters({**basic, 'shear_G': G, 'alpha': 123.})
     parameters = mm.ModelParameters({**basic, 'convergence_K': K})
+    parameters = mm.ModelParameters(
+        {**basic, 'shear_G': G, 'convergence_K': K, 'alpha': 123.})
 
-def test_4():
-    parameters = mm.ModelParameters({**basic, 'shear_G': G, 'convergence_K': K, 'alpha': alpha})
 
-# In each case below, calling mm.ModelParameters() should raise an error.
 class TestParameters(unittest.TestCase):
-    def test_5(self):
-        with self.assertRaises(KeyError):
-            parameters = mm.ModelParameters({'t_0': t0, 'u_0': u0, 'shear_G': G})
+    def test_failing_single_lens_with_mass_sheet(self):
+        """
+        Test if Chang-Refsdal microlensing fails when it's expected to fail.
+        """
+        basic = {'t_0': 1000., 'u_0': 0.1, 't_E': 20.}
+        G = complex(-0.1, -0.2)
+        K = -0.1
+        alpha = 123.
 
-    def test_6(self):
+        with self.assertRaises(KeyError):
+            parameters = mm.ModelParameters(
+                {'t_0': 1000., 'u_0': 0.1, 'shear_G': G})
+        with self.assertRaises(KeyError):
+            parameters = mm.ModelParameters(
+                {'t_E': 20., 'u_0': 0.1, 'shear_G': G})
+        with self.assertRaises(KeyError):
+            parameters = mm.ModelParameters(
+                {'t_0': 1000., 't_E': 20., 'shear_G': G})
+
+        # Cases below have too many parameters:
         with self.assertRaises(KeyError):
             parameters = mm.ModelParameters({**basic, 'alpha': alpha})
-
-    def test_7(self):
         with self.assertRaises(KeyError):
-            parameters = mm.ModelParameters({**basic, 'convergence_K': K, 'alpha': alpha})
-            # This is too much.
+            parameters = mm.ModelParameters(
+                {**basic, 'convergence_K': K, 'alpha': alpha})
+        with self.assertRaises(KeyError):
+            parameters = mm.ModelParameters({
+                **basic, 'convergence_K': K, 'alpha': alpha,
+                'dalpha_dt': -0.3})
+        with self.assertRaises(KeyError):
+            parameters = mm.ModelParameters({
+                **basic, 'shear_G': G, 'convergence_K': K, 'alpha': alpha,
+                'dalpha_dt': -0.3})
 
-    def test_8(self):
+        # The case below is missing alpha:
         with self.assertRaises(KeyError):
             parameters = mm.ModelParameters({**basic, 'shear_G': G})
-            # This is not enough
-
-    def test_9(self):
-        with self.assertRaises(KeyError):
-            parameters = mm.ModelParameters({**basic, 'convergence_K': K, 'alpha': alpha, 'dalpha_dt': da_dt})
-
-    def test_10(self):
-        with self.assertRaises(KeyError):
-            parameters = mm.ModelParameters({**basic, 'shear_G': G, 'convergence_K': K, 'alpha': alpha, 'dalpha_dt': da_dt})
 
