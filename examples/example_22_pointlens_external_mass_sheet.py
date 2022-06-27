705f8dbb290cad9c185a82dd771bc6dd84bb3fc9
radek_poleski <radek.poleski@gmail.com>
2022-06-27 23:15:35 +0200

    ex22 refactored

--- examples/example_22_pointlens_external_mass_sheet.py
+++ examples/example_22_pointlens_external_mass_sheet.py
@@ -16,29 +16,28 @@ G = complex(0.1, -0.05)
 t_0 = 300
 t_E = 500
 u_0 = 0.1
+d_t = 475.
 
-time = np.arange(t_0-775., t_0+775., 0.4, dtype=float)
+time = np.arange(t_0-d_t, t_0+d_t, 0.5, dtype=float)
 
-lens = mm.model.Model({
-    't_0': t_0, 'u_0': u_0, 't_E': t_E,
-    'convergence_K': K, 'shear_G': G, 'alpha': alpha})
-no_shear = mm.model.Model({'t_0': t_0, 'u_0': u_0, 't_E': t_E})
-
+no_shear = mm.Model({'t_0': t_0, 'u_0': u_0, 't_E': t_E})
+lens = mm.Model({**no_shear.parameters.parameters,
+                 'convergence_K': K, 'shear_G': G, 'alpha': alpha})
 lens.set_magnification_methods(
     [min(time), 'point_source_with_shear', max(time)])
-no_shear.set_magnification_methods([min(time), 'point_source', max(time)])
 
 # Plot magnification curve and caustics
 (_, (ax1, ax2)) = plt.subplots(figsize=(10, 5), ncols=2)
 
-ax1.plot(time, lens.get_magnification(time), color='r')
-ax1.plot(time, no_shear.get_magnification(time), alpha=0.4)
+plt.sca(ax1)
+lens.plot_magnification(times=time, color='r')
+no_shear.plot_magnification(times=time, alpha=0.4)
 
-ax2.set_xlim(-1.5, 1.5)
-ax2.set_ylim(-1.5, 1.5)
-ax2.set_xlabel("x", fontweight="bold")
-ax2.set_ylabel("y", fontweight="bold")
-lens.plot_trajectory(t_range=[t_0 - 475, t_0], caustics=True, color='green')
-lens.plot_trajectory(t_range=[t_0, t_0 + 475], color='blue')
+plt.sca(ax2)
+lens.plot_trajectory(t_range=[t_0 - d_t, t_0], caustics=True, color='green')
+lens.plot_trajectory(t_range=[t_0, t_0 + d_t], color='blue')
+plt.xlabel("x", fontweight="bold")
+plt.ylabel("y", fontweight="bold")
+plt.axis('equal')
 
 plt.show()
