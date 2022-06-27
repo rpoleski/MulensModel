944646dfe7b4e6bedb6d1e46dec28400098e79cb
radek_poleski <radek.poleski@gmail.com>
2022-06-27 20:30:56 +0200

    minor in test_Caustics.py

--- source/MulensModel/tests/test_Caustics.py
+++ source/MulensModel/tests/test_Caustics.py
@@ -30,16 +30,18 @@ def test_caustic():
         np.testing.assert_almost_equal(
             y[i], test_caustics['Y'][index], decimal=5)
 
+
 # def test_point_caustic():
 #     """
 #     Test point lens caustics with external mass sheet
 #     """
 #     convergence_K = 0.1
-#     shear_G = complex(-0.1,-0.2)
+#     shear_G = complex(-0.1, -0.2)
 
-#     caustics = mm.CausticsPointWithShear(convergence_K=convergence_K, shear_G=shear_G)
+#     caustics = mm.CausticsPointWithShear(convergence_K=convergence_K,
+#                                          shear_G=shear_G)
 
-#     x, y = caustics.get_caustics(n_points=1000)
+#     (x, y) = caustics.get_caustics(n_points=1000)
 #     for i in range(0, len(x), 100):
 #         index = np.argmin(
 #             np.sqrt((test_caustics['X']-x[i])**2+(test_caustics['Y']-y[i])**2))
@@ -48,6 +50,7 @@ def test_caustic():
 #             x[i], test_caustics['X'][index], decimal=5)
 #         np.testing.assert_almost_equal(
 #             y[i], test_caustics['Y'][index], decimal=5)
+
 
 def test_point_caustic_no_external_mass_sheet():
     """
