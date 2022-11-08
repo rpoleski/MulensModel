f6f3b32964a59654437d4b45aa4fee49391ced98
radek_poleski <radek.poleski@gmail.com>
2022-11-08 19:08:20 +0100

    adding test_init_for_2_sources() in test_ModelParameters.py

--- source/MulensModel/tests/test_ModelParameters.py
+++ source/MulensModel/tests/test_ModelParameters.py
@@ -30,6 +30,20 @@ class TestModelParameters(unittest.TestCase):
             mm.ModelParameters({'t_0': 123., 'u_0': 1, 't_E': 10., 's': 1.2,
                                'alpha': 34.56, 'q': 1.5})
 
+    def test_init_for_2_sources(self):
+        """
+        Test different problems for binary source models that should fail.
+        """
+        with self.assertRaises(Exception):
+            mm.ModelParameters({'t_0_1': 1, 'u_0_1': 0.1, 't_E': 10,
+                                't_0_2': 10., 'u_0_2': 0.01, 'rho_2': -3.})
+        with self.assertRaises(Exception):
+            mm.ModelParameters({'t_0_1': 1, 'u_0_1': 0.1, 't_E': 10, 'rho_1': -0.1,
+                                't_0_2': 10., 'u_0_2': 0.01,})
+        with self.assertRaises(KeyError):
+            mm.ModelParameters({'t_01': 1, 'u_0_1': 0.1, 't_eff_1': 10,
+                                't_0_2': 10., 'u_0_2': 0.01, 't_eff_2': 20.})
+
 def test_init_parameters():
     """
     make sure that parameters are properly stored
