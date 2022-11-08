c7c996499a773fc49157f62e4bc52c7fa6403d58
radek_poleski <radek.poleski@gmail.com>
2022-11-08 18:38:57 +0100

    adding test_point_lens_caustic() and get_index_of_nearest_point()

--- source/MulensModel/tests/test_Caustics.py
+++ source/MulensModel/tests/test_Caustics.py
@@ -4,10 +4,19 @@ import os
 import MulensModel as mm
 
 
-SAMPLE_FILE_01 = os.path.join(
-    mm.DATA_PATH, "unit_test_files", "MB11293_caustics.dat")
+dir_ = os.path.join(mm.DATA_PATH, "unit_test_files")
+FILE_01 = os.path.join(dir_, "MB11293_caustics.dat")
+FILE_02 = os.path.join(dir_, "caustic_point_lens_with_shear.dat")
 
-test_caustics = np.genfromtxt(SAMPLE_FILE_01, names=['X', 'Y'], dtype=None)
+test_caustics = np.genfromtxt(FILE_01, names=['X', 'Y'], dtype=None)
+test_caustics_point = np.genfromtxt(FILE_02, names=['X', 'Y'], dtype=None)
+
+
+def get_index_of_nearest_point(test_data, x, y):
+    """
+    Find index of test_data (*dict*) that is closes to (x, y)
+    """
+    return np.argmin((test_data['X']-x)**2+(test_data['Y']-y)**2)
 
 
 def test_caustic():
@@ -21,8 +30,7 @@ def test_caustic():
 
     x, y = caustics.get_caustics(n_points=1000)
     for i in range(0, len(x), 100):
-        index = np.argmin(
-            np.sqrt((test_caustics['X']-x[i])**2+(test_caustics['Y']-y[i])**2))
+        index = get_index_of_nearest_point(test_caustics, x[i], y[i])
 
         np.testing.assert_almost_equal(
             x[i], test_caustics['X'][index], decimal=5)
@@ -30,25 +38,22 @@ def test_caustic():
             y[i], test_caustics['Y'][index], decimal=5)
 
 
-# def test_point_caustic():
-#     """
-#     Test point lens caustics with external mass sheet
-#     """
-#     convergence_K = 0.1
-#     shear_G = complex(-0.1, -0.2)
+def test_point_lens_caustic():
+    """
+    Test point lens caustics with external mass sheet
+    """
+    convergence_K = 0.1
+    shear_G = complex(-0.1, -0.2)
 
-#     caustics = mm.CausticsPointWithShear(convergence_K=convergence_K,
-#                                          shear_G=shear_G)
+    caustics = mm.CausticsPointWithShear(convergence_K=convergence_K,
+                                         shear_G=shear_G)
 
-#     (x, y) = caustics.get_caustics(n_points=1000)
-#     for i in range(0, len(x), 100):
-#         index = np.argmin(
-#             np.sqrt((test_caustics['X']-x[i])**2+(test_caustics['Y']-y[i])**2))
+    (x, y) = caustics.get_caustics(n_points=1000)
+    for i in range(0, len(x), 100):
+        index = get_index_of_nearest_point(test_caustics_point, x[i], y[i])
 
-#         np.testing.assert_almost_equal(
-#             x[i], test_caustics['X'][index], decimal=5)
-#         np.testing.assert_almost_equal(
-#             y[i], test_caustics['Y'][index], decimal=5)
+        np.testing.assert_almost_equal(x[i], test_caustics_point['X'][index])
+        np.testing.assert_almost_equal(y[i], test_caustics_point['Y'][index])
 
 
 def test_point_caustic_no_external_mass_sheet():
