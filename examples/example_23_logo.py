748d7eb803b234a141513ce5bb1b03eecaabdbbe
jenniferyee <jennifer.c.yee@gmail.com>
2022-12-01 10:41:47 -0500

    ex23: plots two lcs that look like MM.

---
+++ examples/example_23_logo.py
@@ -0,0 +1,17 @@
+import MulensModel as mm
+import matplotlib.pyplot as plt
+
+binary_model = mm.Model(
+    {'t_0': 1.3, 'u_0': 0.11, 't_E': 1.5, 's': 0.5, 'q': 0.3, 'alpha': -38.})
+
+binary_source_model = mm.Model(
+    {'t_0_1': 0.5, 'u_0_1': 0.08, 't_E': 0.5,
+     't_0_2': 0.8, 'u_0_2': 0.11})
+
+plt.figure()
+binary_source_model.plot_lc(
+    source_flux=[1.4, 1.6], blend_flux=18.0, color='blue', lw=10)
+binary_model.plot_lc(
+    source_flux=1.0, blend_flux=20.0, color='magenta', lw=10)
+plt.xlim(0., 2.)
+plt.show()
