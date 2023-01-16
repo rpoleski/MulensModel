3717d1bb231efa8bbcb65dc5be8726a04e34565a
radek_poleski <radek.poleski@gmail.com>
2023-01-16 01:21:30 +0100

    ex24 - headers added

--- examples/example_24_MB08310.py
+++ examples/example_24_MB08310.py
@@ -4,7 +4,6 @@ Created from example_05_MB08310.py
 
 From `Janczak et al. 2010, ApJ 711, 731
 <https://ui.adsabs.harvard.edu/abs/2010ApJ...711..731J/abstract>`_.
-
 """
 import glob
 import os
@@ -23,7 +22,7 @@ for file_ in sorted(files):
     data = mm.MulensData(file_name=file_, comments=["\\", "|"])
     datasets_default.append(data)
 
-# Print dataset information
+print("Printing datasets:")
 for data in datasets_default:
     print(data)
 
@@ -36,15 +35,13 @@ plens_model = mm.Model({'t_0': t_0, 'u_0': u_0, 't_E': t_E, 't_star': t_star})
 method = 'finite_source_uniform_Gould94'
 plens_model.set_magnification_methods([t_0-2.*t_star, method, t_0+2.*t_star])
 
-# Print model information
-print('\n')
+print('\nPrinting model:')
 print(plens_model)
-print('\n')
 
 # Combine the data and model into an event
 event_default = mm.Event(datasets=datasets_default, model=plens_model)
 event_default.data_ref = 6
 
-# Print event information
+print("\nPrinting event:")
 print(event_default)
 
