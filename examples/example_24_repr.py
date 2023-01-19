ba1bb8ebdad6c7cf5a03f62c2d8045c2cb40ac0c
jenniferyee <jennifer.c.yee@gmail.com>
2023-01-19 11:05:54 -0500

    Renamed to be more descriptive. Added properties to show full output.

--- examples/example_24_repr.py
+++ examples/example_24_repr.py
@@ -16,11 +16,27 @@ import MulensModel as mm
 files = glob.glob(os.path.join(mm.DATA_PATH, "photometry_files",
                                "MB08310", "*.tbl"))
 
+coordinates = "17:54:14.53 −34:46:40.99"
+
+filters = {'Auck': 'R', 'Bron': 'R', 'Canopus': 'I', 'CTIO_H': 'H',
+           'CTIO_I': 'I', 'Danish': 'I', 'MOA': 'R'}
+
 # Read in the data
 datasets_default = []
 for file_ in sorted(files):
-    data = mm.MulensData(file_name=file_, comments=["\\", "|"])
+    file_elements = os.path.basename(file_).split('_')
+    name = file_elements[0]
+    if name == 'CTIO':
+        name += '_'
+        name += file_elements[1]
+
+    data = mm.MulensData(
+        file_name=file_, comments=["\\", "|"], bandpass=filters[name])
+    if name == 'MOA':
+        data.scale_errorbars(1.6, 0.001)
+
     datasets_default.append(data)
+
 
 print("Printing datasets:")
 for data in datasets_default:
@@ -34,12 +50,17 @@ t_star = 0.05487
 plens_model = mm.Model({'t_0': t_0, 'u_0': u_0, 't_E': t_E, 't_star': t_star})
 method = 'finite_source_uniform_Gould94'
 plens_model.set_magnification_methods([t_0-2.*t_star, method, t_0+2.*t_star])
+plens_model.set_limb_coeff_u('I', 0.547)
+plens_model.set_limb_coeff_u('V', 0.714)
+plens_model.set_limb_coeff_u('R', 0.633)
+plens_model.set_limb_coeff_u('H', 0.368)
 
 print('\nPrinting model:')
 print(plens_model)
 
 # Combine the data and model into an event
-event_default = mm.Event(datasets=datasets_default, model=plens_model)
+event_default = mm.Event(
+    datasets=datasets_default, model=plens_model, coords=coordinates)
 event_default.data_ref = 6
 
 print("\nPrinting event:")
