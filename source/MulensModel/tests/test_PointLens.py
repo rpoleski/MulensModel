36093fbd58037058a4834b7ad20d693e621c248b
jenniferyee <jennifer.c.yee@gmail.com>
2024-01-22 16:53:34 -0500

    Updated Yoo04Direct tests so db0 raises an error.

--- source/MulensModel/tests/test_PointLens.py
+++ source/MulensModel/tests/test_PointLens.py
@@ -526,6 +526,9 @@ class TestFiniteSourceLDYoo04DirectMagnification(
                 trajectory=trajectory, gamma=gamma, direct=True)
             self.mag_objs.append(mag_obj)
 
+    def test_db0(self):
+        TestFiniteSourceUniformGould94DirectMagnification.test_db0(self)
+
     def test_db1(self):
         for mag_obj in self.mag_objs:
             with self.assertRaises(NotImplementedError):
