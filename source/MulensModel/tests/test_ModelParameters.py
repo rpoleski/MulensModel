41923b8ceb6f559515745ee6a04e78785ca744b2
jenniferyee <jennifer.c.yee@gmail.com>
2024-02-27 11:58:57 -0500

    New unit tests for SourceParameters class.

--- source/MulensModel/tests/test_ModelParameters.py
+++ source/MulensModel/tests/test_ModelParameters.py
@@ -759,3 +759,20 @@ class Test1L3SModelErrors(unittest.TestCase):
                       't_E': 9}
         with self.assertRaises(KeyError):
             model_params = mm.ModelParameters(parameters)
+
+
+class TestSourceParameters(unittest.TestCase):
+
+    def test_1(self):
+        parameters = {'t_0': 0, 'u_0': 1, 't_E': 9}
+        src_par = mm.SourceParameters(parameters)
+        for key, value in parameters.items():
+            assert src_par.__getattr__(key) == value
+
+    def test_2(self):
+        parameters = {'t_0': 0, 'u_0': 1, 't_E': 9, 'rho': 0.001}
+        src_par = mm.SourceParameters(parameters)
+        for key, value in parameters.items():
+            assert src_par.__getattr__(key) == value
+
+        assert src_par.t_star == parameters['t_E'] * parameters['rho']
