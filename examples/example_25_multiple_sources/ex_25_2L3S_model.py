b62d7a8c0a3a17f911e375f82bb0b1f80b28e04f
jenniferyee <jennifer.c.yee@gmail.com>
2024-08-13 13:32:20 -0400

    2L3S+Par+OBT model implemented.

--- examples/example_25_multiple_sources/ex_25_2L3S_model.py
+++ examples/example_25_multiple_sources/ex_25_2L3S_model.py
@@ -18,13 +18,15 @@ t_E = 4.613
 s = 0.857
 q = 2.374e-3
 alpha = -np.rad2deg(0.890)
-params = {
+params_static = {
     't_E': t_E, 's': s, 'q': q, 'alpha': alpha,
     't_0_1': 7199.941, 'u_0_1': 0.069, 'rho_1': 0.688e-3,
     't_0_2': 7200.057, 'u_0_2': 0.039, 'rho_2': 0.542e-3,
-    't_0_3': 7197., 'u_0_3': -0.28, 'rho_3': 2.e-3,
-    'pi_E_E': 0.3, 'pi_E_N': 0.1,
-    'ds_dt': 0.5, 'dalpha_dt': 15., 't_0_kep': 7200.2}
+    't_0_3': 7197., 'u_0_3': -0.28, 'rho_3': 2.e-3}
+
+params = {**params_static,
+    'pi_E_E': 100.0, 'pi_E_N': 1.0,
+    'ds_dt': 3.0, 'dalpha_dt': 360., 't_0_kep': 7200.2}
 
 q_F_2 = 0.181
 source_flux_1 = 0.061
@@ -40,6 +42,12 @@ model = mm.Model(params, coords=coords)
 model.set_magnification_methods(mag_methods_12, 1)
 model.set_magnification_methods(mag_methods_12, 2)
 model.set_magnification_methods(mag_methods_3, 3)
+print(model)
+
+model_static = mm.Model(params_static, coords=coords)
+model_static.set_magnification_methods(mag_methods_12, 1)
+model_static.set_magnification_methods(mag_methods_12, 2)
+model_static.set_magnification_methods(mag_methods_3, 3)
 
 # Make the plots
 times_perturb_1 = np.arange(7200.1, 7200.3, 0.001)
@@ -50,13 +58,17 @@ times.sort()
 
 plt.figure()
 model.plot_trajectory(caustics=True)
+model.plot_caustics(epoch=7196., color='blue')
 plt.gca().set_aspect('equal')
 plt.xlim(-0.5, 0.5)
 plt.ylim(-0.5, 0.5)
 plt.minorticks_on()
 
 plt.figure()
-model.plot_lc(times, source_flux=source_fluxes, blend_flux=blend_flux)
+model_static.plot_lc(
+    times, label='static', color='gray', linestyle=':', source_flux=source_fluxes, blend_flux=blend_flux)
+model.plot_lc(times, label='full', source_flux=source_fluxes, blend_flux=blend_flux)
+plt.legend()
 plt.minorticks_on()
 plt.tight_layout()
 
