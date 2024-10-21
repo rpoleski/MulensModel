4eb10b22d46125c7f394193850e04d89e688e1d1
jenniferyee <jennifer.c.yee@gmail.com>
2024-10-21 10:56:07 -0400

    added errorbars.

--- examples/example_26_pymc.py
+++ examples/example_26_pymc.py
@@ -14,7 +14,6 @@ import arviz as az
 import numpy as np
 import MulensModel as mm
 
-"""
 # JCY: For now, we're going to ignore bad data
 file_names = ['../data/photometry_files/MB08310/MOA_0300089_PLC_007.tbl',
               '../data/photometry_files/MB08310/CTIO_H_0300089_PLC_004.tbl',
@@ -24,19 +23,24 @@ data = [mm.MulensData(file_name=name, **kwargs) for name in file_names]
 
 parameters = {'t_0': 2454656.4, 'u_0': 0.003, 't_E': 11.14, 't_star': 0.055}
 model = mm.Model(parameters)
+model.set_magnification_methods(
+    [2454656.25, 'finite_source_uniform_Gould94', 2454656.55])
 
 event = mm.Event(datasets=data, model=model)
 
 # New pymc stuff:
+parameters_to_fit = ['t_0', 'u_0', 't_E', 't_star']
 basic_model = pm.Model()
 
 observed_fluxes = None
+observed_errors = None
 for dataset in event.datasets:
     if observed_fluxes is None:
         observed_fluxes = dataset.flux
+        observed_errors = dataset.err_flux
     else:
         observed_fluxes = np.hstack([observed_fluxes, dataset.flux])
-
+        observed_errors = np.hstack([observed_errors, dataset.err_flux])
 
 with basic_model:
     # Priors for unknown model parameters
@@ -45,16 +49,18 @@ with basic_model:
     t_E = pm.Normal('t_E', mu=11.14, sigma=0.05)
     t_star = pm.Normal('t_star', mu=0.055, sigma=0.001)
 
+    event.fit_fluxes()
+
     # Expected value of outcome
     mod_fluxes = None
-    for dataset in event.datasets:
+    for fit in event.fits:
         if mod_fluxes is None:
-            mod_fluxes = event.get_flux_for_dataset(dataset)
+            mod_fluxes = fit.get_model_fluxes()
         else:
-            mod_fluxes = np.hstack([mod_fluxes, event.get_flux_for_dataset(dataset)])
+            mod_fluxes = np.hstack([mod_fluxes, fit.get_model_fluxes()])
 
     # Likelihood (sampling distribution) of observations
-    Y_obs = pm.Normal("Y_obs", mu=mod_fluxes, observed=observed_fluxes)
+    Y_obs = pm.Normal("Y_obs", mu=mod_fluxes, sigma=observed_errors, observed=observed_fluxes)
 
 with basic_model:
     # draw 1000 posterior samples
@@ -62,4 +68,3 @@ with basic_model:
 
 az.summary(idata)
 az.plot_trace(idata, combine=True)
-"""
