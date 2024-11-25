0a697e07bab791f0c4bc91302c32af51efb3540d
radek_poleski <radek.poleski@gmail.com>
2024-11-25 07:19:02 +0100

    minor in use cases

--- examples/use_cases/use_case_14_coordinate_system.py
+++ examples/use_cases/use_case_14_coordinate_system.py
@@ -58,7 +58,7 @@ print('Center of mass: {0}, {1}'.format(t0_center_of_mass, u0_center_of_mass))
 # Alternatively,
 model = mm.Model(
             {'t_0': 2457000., 'u_0': 0.1, 't_E': 30., 'rho': 0.001,
-             'alpha': 30*u.deg, 's': 1.1, 'q': 0.001},
+             'alpha': 30, 's': 1.1, 'q': 0.001},
             frame_origin='magnification')
 
 print(model.parameters.t_0, model.parameters.u_0)
