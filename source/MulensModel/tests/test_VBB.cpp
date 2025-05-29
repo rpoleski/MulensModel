83eb4037d107e4d6c3127f2cf622c0f7f704d5d2
SophieBudzik <sophiebudzik@gmail.com>
2025-05-29 16:10:55 +0200

    test_changes

--- source/MulensModel/tests/test_VBB.cpp
+++ source/MulensModel/tests/test_VBB.cpp
@@ -12,9 +12,9 @@ int main()
         double s, q, Rs;
 
         s = 1.2; //separation between the two lenses^M
-        q = 0.123;
+        q = 0.123; //mass ratio of the lens components
         u0 = 0.555; // Impact parameter^M
-        alpha = 17.5 * M_PI/180; // Angle between a vector pointing to the left and the source velocity^M
+        alpha = 17.5 * M_PI/180; // Angle between a vector pointing to the left and the source velocity [radians]
         t0 = 5500; // Time of closest approach to the center of mass^M
         tE = 100; // Einstein time^M
         Rs = 0.01; // Source radius in Einstein radii of the total mass.^M
@@ -29,7 +29,7 @@ int main()
 
         const int np = 5;
         double t_array[np];
-
+        // making the array of times
         for (int i = 0; i < np; i++) {
                 t_array[i] = t0 - 3 * tE + i * (6 * tE / (np - 1));
         }
@@ -37,22 +37,32 @@ int main()
 	
         VBBL.SetObjectCoordinates("18:00:00 -26:00:00");
 
-	double w1, w2, w3;
-        w1 = 2.8725325543944145e-05;
-        w2 = -9.563447956133312e-06;
-        w3 = 0.00011048202132286211;
+	double ds_dt, dalpha_dt, ds_z_dt;
+	double s_z, s_prim, conv_factor;
+	double ds_dt_VBB, da_dt_VBB, dsz_dt_VBB;
 
-        pr[9] = w1;
-        pr[10] = w2;
-        pr[11] = w3;
+	ds_dt = 0.13; //the rate of change of the separation per year
+        dalpha_dt = -0.2; //rate of change of alpha deg per year
+        ds_z_dt = 0.5; // rate of change of the distance along the line of sight per year
+
+	conv_factor = -ds_dt * s;
+        s_z = conv_factor/ds_z_dt; //distance along the line of sight
+        s_prim = sqrt(pow(s,2) + pow(s_z,2)); 
+
+        ds_dt_VBB = ds_dt * 1/s_prim * 1/365; // the rate of change of the separation per day
+        da_dt_VBB = dalpha_dt * M_PI/180 * 1/365; // rate of change of alpha radian per day
+        dsz_dt_VBB = ds_z_dt * 1/s_prim * 1/365; // rate of change of the distance along the line of sight per year
+
+
+        pr[9] = ds_dt_VBB;
+        pr[10] = da_dt_VBB;
+        pr[11] = dsz_dt_VBB;
 
 	double mag_orbital_array[np], y1_orbital_array[np], y2_orbital_array[np], sep_array[np];
+	// calculating the trajectory 
         VBBL.BinaryLightCurveOrbital(pr, t_array, mag_orbital_array, y1_orbital_array, y2_orbital_array, sep_array, np);
         
 
-        //for (int i = 0; i < np; i++) {
-                //printf("%lf  %lf %lf\n", t_array[i], y1_orbital_array[i], y2_orbital_array[i]);
-        //}
 	printf("[%lf, %lf, %lf, %lf, %lf]\n", y1_orbital_array[0], y1_orbital_array[1], y1_orbital_array[2], y1_orbital_array[3], y1_orbital_array[4]);
 	printf("[%lf, %lf, %lf, %lf, %lf]\n", y2_orbital_array[0], y2_orbital_array[1], y2_orbital_array[2], y2_orbital_array[3], y2_orbital_array[4]);
 
--- source/MulensModel/tests/test_compare_trajectory.py
+++ source/MulensModel/tests/test_compare_trajectory.py
@@ -3,49 +3,37 @@ import numpy as np
 import unittest
 import pytest
 
-s = 1.2
-q = 0.123
-u_0 = 0.555
-alpha = 17.5
-t_0 = 2455500
-t_E = 100
-rho = 0.01
 
-
-ds_dt = 0.013
-dalpha_dt = -0.2
-ds_z_dt = 0.05
-
-N = 5
-times = []
-
-for i in range(0,N,1):
-    times.append(t_0 - 3 * t_E + i * (6 * t_E / (N - 1)))
-
-
-parameters_classic = mm.ModelParameters({'t_0': t_0, 'u_0': u_0, 't_E': t_E, 's': s, 'q': q, 'alpha': alpha,'rho': rho})
-
-parameters_extra = mm.ModelParameters({'t_0': t_0, 'u_0': u_0, 't_E': t_E, 's': s, 'q': q, 'alpha': alpha,'rho': rho, 'ds_dt': ds_dt, 'dalpha_dt': dalpha_dt, 'ds_z_dt': ds_z_dt})
-
-def unit_change_for_VBB(ds_dt_MM, da_dt_MM, dsz_dt_MM):
-    s_z = parameters_extra.s_z
-    s_prim = np.sqrt(s**2 + s_z**2)
-    
-    ds_dt_VBB = ds_dt_MM * 1/s_prim * 1/365
-    da_dt_VBB = da_dt_MM * np.pi/180 * 1/365
-    dsz_dt_VBB = dsz_dt_MM * 1/s_prim * 1/365
-    return ds_dt_VBB, da_dt_VBB, dsz_dt_VBB
-
-#print(unit_change_for_VBB(ds_dt, dalpha_dt, ds_z_dt))
-
-def test_init_parameters():
+def test_trajectory():
     """
-    compare trajectory
+    compares trajectory to values form VBBinaryLensing
     """
+
+
+    s = 1.2
+    q = 0.123
+    u_0 = 0.555
+    alpha = 17.5
+    t_0 = 2455500
+    t_E = 100
+    rho = 0.01
+
+    ds_dt = 0.13
+    dalpha_dt = -0.2
+    ds_z_dt = 0.5
+
+    N = 5
+    times = []
+
+    for i in range(0,N,1):
+        times.append(t_0 - 3 * t_E + i * (6 * t_E / (N - 1)))
+
+    parameters_extra = mm.ModelParameters({'t_0': t_0, 'u_0': u_0, 't_E': t_E, 's': s, 'q': q, 'alpha': alpha,'rho': rho, 'ds_dt': ds_dt, 'dalpha_dt': dalpha_dt, 'ds_z_dt': ds_z_dt})
+
     trajectory = mm.Trajectory(parameters=parameters_extra, times=times)
     
-    x_VBB = [3.026951, 1.597578, 0.166892, -1.265083, -2.698321]
-    y_VBB = [0.381569, -0.075952, -0.529313, -0.978565, -1.423758]
+    x_VBB = [3.026807, 1.597584, 0.166892, -1.265043, -2.698163]
+    y_VBB = [0.382709, -0.075836, -0.529313, -0.978617, -1.424058] 
 
     np.testing.assert_almost_equal(trajectory.x, x_VBB)
     np.testing.assert_almost_equal(trajectory.y, y_VBB)
