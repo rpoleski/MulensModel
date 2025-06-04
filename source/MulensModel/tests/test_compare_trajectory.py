705007358cde10db4c28fbd285e6d6972877c07b
Zofia <89477661+SophieBudzik@users.noreply.github.com>
2025-06-04 12:54:16 +0200

    optimizing

--- source/MulensModel/tests/test_compare_trajectory.py
+++ source/MulensModel/tests/test_compare_trajectory.py
@@ -4,11 +4,17 @@ import unittest
 import pytest
 
 
-def test_trajectory():
-    """
-    compares trajectory to values form VBBinaryLensing
-    """
+def get_times():
+    N = 5
+    times = []
+    t_0 = get_parameters().t_0
+    t_E = get_parameters().t_E
 
+    for i in range(0,N,1):
+        times.append(t_0 - 3 * t_E + i * (6 * t_E / (N - 1)))
+    return times
+
+def get_parameters():
 
     s = 1.2
     q = 0.123
@@ -22,15 +28,20 @@ def test_trajectory():
     dalpha_dt = -30.3
     ds_z_dt = 20
 
-    N = 5
-    times = []
-
-    for i in range(0,N,1):
-        times.append(t_0 - 3 * t_E + i * (6 * t_E / (N - 1)))
 
     parameters_extra = mm.ModelParameters({'t_0': t_0, 'u_0': u_0, 't_E': t_E, 's': s, 'q': q, 'alpha': alpha,'rho': rho, 'ds_dt': ds_dt, 'dalpha_dt': dalpha_dt, 'ds_z_dt': ds_z_dt})
 
-    trajectory = mm.Trajectory(parameters=parameters_extra, times=times)
+    return parameters_extra
+
+
+def test_trajectory():
+    """
+    compares trajectory to values form VBBinaryLensing
+    """
+    times = get_times()
+    parameters = get_parameters()
+
+    trajectory = mm.Trajectory(parameters=parameters, times=times)
 
     x_VBB = [-3.023197, 1.593006, 0.166892, 1.310978, 2.705571]
     y_VBB = [-0.410250, -0.142673, -0.529313, 0.916167, 1.409933]
