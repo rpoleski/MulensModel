7db8899205d6e23d4a900b99954cfc7f4e178f6c
SophieBudzik <sophiebudzik@gmail.com>
2026-06-19 16:43:10 +0200

    2 lines back

--- examples/example_16/ulens_model_fit.py
+++ examples/example_16/ulens_model_fit.py
@@ -1499,7 +1499,8 @@ class UlensModelFit(object):
                         conversion[key] = "|" + conversion[key] + "|"
 
         self._fit_parameters_latex = [('$' + conversion[key] + '$') for key in self._fit_parameters]
-
+        if self._extra_parameters is not None:
+            self._extra_parameters_latex = [('$' + conversion[key] + '$') for key in self._extra_parameters]
     def _parse_fitting_parameters(self):
         """
         run some checks on self._fitting_parameters to make sure that
